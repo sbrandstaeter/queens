1ee5813a284a2138d4110566e4f2932a1e7e3101
jbi <biehler@adco-engineering-gw.com>
2017-05-05 08:46:21 +0200

    added test casess for KLE based constructor

--- pqueens/tests/test_random_field_gen_fourier_constructor.py
+++ pqueens/tests/test_random_field_gen_fourier_constructor.py
@@ -131,6 +131,71 @@ class TestRandomFieldGeneratorFourierConstruction(unittest.TestCase):
             self.num_terms_per_dim,
             self.total_terms)
 
+class TestRandomFieldGeneratorKLEConstruction(unittest.TestCase):
+
+    def setUp(self):
+        # setup some necessary variables to setup random field generators
+        self.dimension               = 1
+        self.corrstruct              = 'exp'
+        self.corr_length             = 25
+        self.energy_frac             = 0.95
+        self.field_bbox              = np.array([-100, 100])
+        self.marginal_pdf            = norm(0,1)
+        self.num_terms_per_dim       = 120
+        self.total_terms             = 120
+        self.loc                     = [0, 10, 25, 100]
+        self.seed                    = 42
+
+    #raise RuntimeError('field bounding box must be size {} and not {}'.format(self.spatial_dim*2,san_check_bbox[0]))
+    def test_construction_boundingbox_dimension(self):
+        with self.assertRaises(ValueError):
+            mystuff = UniVarRandomFieldGeneratorFactory.create_new_random_field_generator(
+            self.marginal_pdf,
+            self.dimension,
+            self.corrstruct ,
+            self.corr_length,
+            self.energy_frac,
+            np.array([100, 100, 100]),
+            self.num_terms_per_dim,
+            self.total_terms)
+
+    #raise ValueError('energy fraction must be between 0 and 1.')
+    def test_construction_wrong_engergy_frac(self):
+        with self.assertRaises(ValueError):
+            mystuff = UniVarRandomFieldGeneratorFactory.create_new_random_field_generator(
+            self.marginal_pdf,
+            self.dimension,
+            self.corrstruct ,
+            self.corr_length,
+            1.1,
+            self.field_bbox ,
+            self.num_terms_per_dim,
+            self.total_terms)
+
+            mystuff = UniVarRandomFieldGeneratorFactory.create_new_random_field_generator(
+            self.marginal_pdf,
+            self.dimension,
+            self.corrstruct,
+            self.corr_length,
+            -1.1,
+            self.field_bbox ,
+            self.num_terms_per_dim,
+            self.total_terms)
+            mystuff.gen_sample_gauss_field(10,np.array((4,4)))
+
+
+    #raise RuntimeError('Error: correlation length must be positive')
+    def test_construction_correlation_length_negative(self):
+        with self.assertRaises(ValueError):
+            mystuff = UniVarRandomFieldGeneratorFactory.create_new_random_field_generator(
+            self.marginal_pdf,
+            self.dimension,
+            self.corrstruct,
+            -10,
+            self.energy_frac,
+            self.field_bbox ,
+            self.num_terms_per_dim,
+            self.total_terms)
 
 if __name__ == '__main__':
     unittest.main()
