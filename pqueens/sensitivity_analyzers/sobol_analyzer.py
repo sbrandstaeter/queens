f5d96e441cfac005bdd1b38195ccff67be91e7be
Jonas Biehler <biehler@adco-engineering-gw.com>
2017-07-13 11:24:41 +0000

    sobol_analyzer.py edited online with Bitbucket

--- pqueens/sensitivity_analyzers/sobol_analyzer.py
+++ pqueens/sensitivity_analyzers/sobol_analyzer.py
@@ -33,7 +33,6 @@ def __init__(self,problem,calc_second_order=True,num_bootstrap_samples=100,
     self.calc_second_order = calc_second_order
     self.num_bootstrap = num_bootstrap_samples
     self.confidence_level  = confidence_level
-    print("hello world Celcile")
 
     # TODO extract the relvant information form the problem dict. 
 
