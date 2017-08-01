c4e03202dc815c3acdd2f43961b7d9fb375ddeea
Cecile Haberstich (Biehler) <haberstich@red46.stud.lnm.mw.tum.de>
2017-08-01 14:16:25 +0200

    Final version for several realizatins of Gaussian processes for example

--- pqueens/sensitivity_analyzers/sobol_analyzer.py
+++ pqueens/sensitivity_analyzers/sobol_analyzer.py
@@ -4,6 +4,7 @@ import os
 import combi
 import pyDOE
 import random
+from scipy.stats import norm
 
 class SobolAnalyzer(object):
     """ Class for computing Sobol indices
@@ -36,7 +37,7 @@ class SobolAnalyzer(object):
     """
 
     def __init__(self,params,calc_second_order, num_bootstrap_samples,
-                confidence_level, output_samples):
+                confidence_level, output_samples, num_bootstrap_conf):
         """
         Args:
         params (dict):
@@ -50,12 +51,16 @@ class SobolAnalyzer(object):
             The confidence interval level (default 0.95)
         output_samples (int):
             The number of output samples Y (default 1)
+        num_bootstrap_conf (int) :
+            Number of bootstrap iterations for the computation of confidence intervals
         """
         self.params = params
+        self.dim = len(self.params['X'])
         self.calc_second_order = calc_second_order
         self.num_bootstrap_samples = num_bootstrap_samples
         self.confidence_level  = confidence_level
         self.output_samples = output_samples
+        self.num_bootstrap_conf = num_bootstrap_conf
 
     def generate_bootstrap_samples(self,m):
         """ Generate the samples necessary to make bootstrapping """
@@ -70,65 +75,75 @@ class SobolAnalyzer(object):
             H[z,:] = np.round(H[z,:])
         return H
 
-    def compute_first_order_sensitivity_indice(self,Y,Y_tilde):
+    def compute_first_order_sensitivity_indice(self,Y,Y_tilde,m):
         """ Compute the First-Order Indice Sensitivity Indices with
         Algorithm 1 and Proposition 1 in [1] """
         # First order estimator following Le Gratiet et al. 2014
         S_M_N_K_L= np.zeros((self.output_samples,self.num_bootstrap_samples),dtype=float)
-        H = self.generate_bootstrap_samples(len(Y))
-        m = len(Y)
+        H = self.generate_bootstrap_samples(m)
         for k in range(self.output_samples):
-            num = (np.sum(Y*Y_tilde)/m-(np.sum(Y+Y_tilde)/(2*m))**2)
-            den = (sum(Y**2)/m-(np.sum(Y+Y_tilde)/(2*m))**2)
+            num = (np.sum(Y[k,:]*Y_tilde[k,:])/m-(np.sum(Y[k,:]+Y_tilde[k,:])/(2*m))**2)
+            den = (sum(Y[k,:]**2)/m-(np.sum(Y[k,:]+Y_tilde[k,:])/(2*m))**2)
             S_M_N_K_L[k,0] = num/den
             for l in range(1,self.num_bootstrap_samples):
-                Y_b = Y[H[l,:]]
-                Y_tilde_b = Y_tilde[H[l,:]]
+                Y_b = Y[k,H[l,:]]
+                Y_tilde_b = Y_tilde[k,H[l,:]]
                 num_b = (np.sum(Y_b*Y_tilde_b)/m-(np.sum(Y_b+Y_tilde_b)/(2*m))**2)
                 den_b = (sum(Y_b**2)/m-(np.sum(Y_b+Y_tilde_b)/(2*m))**2)
                 S_M_N_K_L[k,l] = num_b/den_b
-        return np.mean(S_M_N_K_L)
+        return S_M_N_K_L
 
-    def compute_second_order_sensitivity_indice(self,Y, Y_tilde, Y_tilde_bis, Y_tilde_ter):
+    def compute_second_order_sensitivity_indice(self,Y, Y_tilde, Y_tilde_bis, Y_tilde_ter,m):
         """ Compute the Second-Order Indice Sensitivity Indices with
         Algorithm 1 and Proposition 1 in [1] """
         S_M_N_K_L =  np.zeros((self.output_samples,self.num_bootstrap_samples), dtype = float)
-        H = self.generate_bootstrap_samples(len(Y))
-        m = len(Y)
+        H = self.generate_bootstrap_samples(m)
         for k in range(self.output_samples):
-            num = (np.sum(Y*Y_tilde)/m-(np.sum(Y+Y_tilde)/(2*m))**2)
-            den = (sum(Y**2)/m-(np.sum(Y+Y_tilde)/(2*m))**2)
-            S_1 = self.compute_first_order_sensitivity_indice(Y, Y_tilde_bis)
-            S_2 = self.compute_first_order_sensitivity_indice(Y, Y_tilde_ter)
+            num = (np.sum(Y[k,:]*Y_tilde[k,:])/m-(np.sum(Y[k,:]+Y_tilde[k,:])/(2*m))**2)
+            den = (sum(Y[k,:]**2)/m-(np.sum(Y[k,:]+Y_tilde[k,:])/(2*m))**2)
+            S_1 = np.mean(self.compute_first_order_sensitivity_indice(Y, Y_tilde_bis,m))
+            S_2 = np.mean(self.compute_first_order_sensitivity_indice(Y, Y_tilde_ter,m))
             S_M_N_K_L[k,0] = num/den - S_1 - S_2
             for l in range(1,self.num_bootstrap_samples):
-                Y_b = Y[H[l,:]]
-                Y_tilde_b = Y_tilde[H[l,:]]
-                Y_tilde_bis_b = Y_tilde_bis[H[l,:]]
-                Y_tilde_ter_b = Y_tilde_ter[H[l,:]]
-                num_b = (np.sum(Y_b*Y_tilde_b)/m-(np.sum(Y_b+Y_tilde_b)/(2*m))**2)
-                den_b = (sum(Y_b**2)/m-(np.sum(Y_b+Y_tilde_b)/(2*m))**2)
-                S_1_b = self.compute_first_order_sensitivity_indice(Y_b, Y_tilde_bis_b)
-                S_2_b = self.compute_first_order_sensitivity_indice(Y_b, Y_tilde_ter_b)
+                Y_b = Y[:,H[l,:]]
+                Y_tilde_b = Y_tilde[:,H[l,:]]
+                Y_tilde_bis_b = Y_tilde_bis[:,H[l,:]]
+                Y_tilde_ter_b = Y_tilde_ter[:,H[l,:]]
+                num_b = (np.sum(Y_b[k,:]*Y_tilde_b[k,:])/m-(np.sum(Y_b[k,:]+Y_tilde_b[k,:])/(2*m))**2)
+                den_b = (sum(Y_b[k,:]**2)/m-(np.sum(Y_b[k,:]+Y_tilde_b[k,:])/(2*m))**2)
+                S_1_b = np.mean(self.compute_first_order_sensitivity_indice(Y_b, Y_tilde_bis_b,m))
+                S_2_b = np.mean(self.compute_first_order_sensitivity_indice(Y_b, Y_tilde_ter_b,m))
                 S_M_N_K_L[k,l] = num_b/den_b - S_1_b - S_2_b
-        return np.mean(S_M_N_K_L)
+        return S_M_N_K_L
 
-    def compute_total_order_sensitivity_indice(self,Y, Y_tilde):
+    def compute_total_order_sensitivity_indice(self,Y, Y_tilde,m):
         """ Compute the Total-Order Indice Sensitivity Indices [2] """
         S_M_N_K_L = np.zeros((self.output_samples,self.num_bootstrap_samples),dtype=float)
-        H = self.generate_bootstrap_samples(len(Y))
-        m = len(Y)
+        H = self.generate_bootstrap_samples(m)
         for k in range(self.output_samples):
-            num = (np.sum(Y*Y_tilde)/m-(np.sum(Y+Y_tilde)/(2*m))**2)
-            den = (sum(Y**2)/m-(np.sum(Y+Y_tilde)/(2*m))**2)
+            num = (np.sum(Y[k,:]*Y_tilde[k,:])/m-(np.sum(Y[k,:]+Y_tilde[k,:])/(2*m))**2)
+            den = (sum(Y[k,:]**2)/m-(np.sum(Y[k,:]+Y_tilde[k,:])/(2*m))**2)
             S_M_N_K_L[k,0] = 1-num/den
             for l in range(1,self.num_bootstrap_samples):
-                Y_b = Y[H[l,:]]
-                Y_tilde_b = Y_tilde[H[l,:]]
+                Y_b = Y[k,H[l,:]]
+                Y_tilde_b = Y_tilde[k,H[l,:]]
                 num_b = (np.sum(Y_b*Y_tilde_b)/m-(np.sum(Y_b+Y_tilde_b)/(2*m))**2)
                 den_b = (sum(Y_b**2)/m-(np.sum(Y_b+Y_tilde_b)/(2*m))**2)
                 S_M_N_K_L[k,l] = 1-num_b/den_b
-        return np.mean(S_M_N_K_L)
+        return S_M_N_K_L
+
+    def compute_confidence_interval(self,conf_level, S_M_N_K_L, num_bootstrap_conf):
+        """Function to compute the confidence intervals for our sensitivity
+        indice"""
+        S_M_N_K_L_bootstrap = np.zeros([self.num_bootstrap_samples*self.output_samples])
+        data_bootstrap = np.zeros([num_bootstrap_conf])
+        if not 0 < conf_level < 1:
+            raise ValueError("Confidence level must be between 0 and 1")
+        S_M_N_K_L_temp = S_M_N_K_L.reshape((1,self.num_bootstrap_samples*self.output_samples))
+        bootstrap_index = np.random.randint(len(S_M_N_K_L_temp), size = (self.num_bootstrap_samples*self.output_samples,num_bootstrap_conf))
+        S_M_N_K_L_bootstrap= S_M_N_K_L[bootstrap_index]
+        data_bootstrap = np.average(np.abs(S_M_N_K_L_bootstrap), axis = 1)
+        return norm.ppf(0.5 + conf_level/2)*data_bootstrap.std(ddof = 1)
 
     def create_Si_dict(self,dim):
         """ Create a dictionnary to store the results, function from
@@ -149,24 +164,47 @@ class SobolAnalyzer(object):
         Returns (dict) :
             dictionary with sensitivity indices
         """
-        # determining the dimension of the problem
-        dim = len(self.params['X'])
         Y = np.asarray(Y)
-        m = len(Y)
-        S = self.create_Si_dict(dim)
-        nb_combi = (dim+2+math.factorial(dim)//(2*math.factorial(dim-2)))
+        m = len(Y[0,:,:])
+        S = self.create_Si_dict(self.dim)
+        nb_combi = (self.dim+2+math.factorial(self.dim)//(2*math.factorial(self.dim-2)))
 
-        for j in range(dim):
-            S['S1'][j] = self.compute_first_order_sensitivity_indice(Y[:,0], Y[:,j+1])
-            S['S1_conf'][j] = 0
-            S['ST'][j] = self.compute_total_order_sensitivity_indice(Y[:,0], Y[:,nb_combi-j-2])
-            S['ST_conf'][j] = 0
+        for j in range(self.dim):
+            S_M_N_K_L = self.compute_first_order_sensitivity_indice(Y[:,:,0], Y[:,:,j+1],m)
+            S['S1'][j] = np.mean(S_M_N_K_L)
+            S['S1_conf'][j] = self.compute_confidence_interval(self.confidence_level, S_M_N_K_L, self.num_bootstrap_conf)
+            S_M_N_K_L_total = self.compute_total_order_sensitivity_indice(Y[:,:,0], Y[:,:,nb_combi-j-2],m)
+            S['ST'][j] = np.mean(S_M_N_K_L_total)
+            S['ST_conf'][j] = self.compute_confidence_interval(self.confidence_level, S_M_N_K_L_total, self.num_bootstrap_conf)
 
         # Second order (+conf.)
         if self.calc_second_order:
-            for j in range(dim):
-                for k in range(j + 1, dim):
-                    S['S2'][j, k] = self.compute_second_order_sensitivity_indice(Y[:,0],
-                    Y[:,j+k+dim], Y[:,j+1], Y[:,k+1])
-                    S['S2_conf'][j, k] =  0
-        return S
+            for j in range(self.dim):
+                for k in range(j + 1, self.dim):
+                    S_M_N_K_L_2 = self.compute_second_order_sensitivity_indice(Y[:,:,0],
+                    Y[:,:,j+k+self.dim], Y[:,:,j+1], Y[:,:,k+1],m)
+                    S['S2'][j, k] = np.mean(S_M_N_K_L_2)
+                    S['S2_conf'][j, k] = self.compute_confidence_interval(self.confidence_level, S_M_N_K_L_2, self.num_bootstrap_conf)
+
+        print("{0:<30} {1:>10} {2:>10} {3:>15} {4:>10}".format(
+            "Parameter",
+            "S1",
+            "S1_conf",
+            "ST",
+            "ST_conf")
+        )
+        for j in list(range(self.dim)):
+            print("{0!s:30} {1!s:10} {2!s:10} {3!s:15} {4!s:10}".format(
+                print('x',j),
+                S['S1'][j],
+                S['S1_conf'][j],
+                S['ST'][j],
+                S['ST_conf'][j]))
+
+        if calc_second_order:
+            print('\n%s_1 %s_2 S2 S2_conf' % (title,title))
+
+        for j in list(range(self.dim))::
+            for k in list(range(j + 1, self.dim)):
+                print("%s %s %f %f" % (names[j], names[k],
+                S['S2'][j, k], S['S2_conf'][j, k]))
