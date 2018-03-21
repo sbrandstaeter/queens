555bb294f314657282da566436ba7352004d287d
jbi <biehler@adco-engineering-gw.com>
2018-03-21 16:27:36 +0100

    added package documentation for database package

---
+++ pqueens/database/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+------------
+Database
+------------
+This package in essence contains a module to store data related to computer experiments
+in a mongodb based database. The class in the module provides a convenience layer
+around certain functions which are needed in QUEENS to read and write input/ouput
+data of simulation models.
+"""
