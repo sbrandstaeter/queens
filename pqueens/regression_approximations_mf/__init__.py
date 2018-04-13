151071f139375143c37f3411e1b96504b6922b34
jbi <biehler@adco-engineering-gw.com>
2018-04-13 09:29:31 +0200

    Inititialize new module for multi-fidelity regression approximations.

---
+++ pqueens/regression_approximations_mf/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Multi-Fidelity Regression Approximations.
+
+This package contains a set of multi-fidelits regression approximations,
+i.e., multi-task regression models which are the essential building block for
+multi-fidelity surrogate models. For the actual implementation of the regression
+models external third party libraries are used, such as `GPy`_, `GPFlow`_.
+
+ .. _GPy:
+     https://github.com/SheffieldML/GPy
+ .. _GPFlow:
+     https://github.com/GPflow/GPflow
+
+"""
