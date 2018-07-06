b14aa2fcf7bb90a07752ea2e2e434fec43f6b63b
jbi <biehler@adco-engineering-gw.com>
2018-07-06 13:28:25 +0200

    Introduced first function test using pytest as testing framework.

---
+++ pqueens/tests/function_tests/test_branin_gp_surrogate.py
@@ -0,0 +1,14 @@
+from pqueens.main import main
+import pytest
+import pickle
+
+def test_branin_gp_surrogate(tmpdir):
+    """ Test case for GP based surrogate model """
+    arguments = ['--input=pqueens/tests/function_tests/input_files/branin_gp_surrogate.json',
+                 '--output='+str(tmpdir)]
+
+    main(arguments)
+    result_file = str(tmpdir)+'/'+'xxx.pickle'
+    with open(result_file, 'rb') as handle:
+        results = pickle.load(handle)
+    assert results["pdf_estimate"]["mean"][1] == pytest.approx(8.860833411995653e-05)
