b1f8b6562f4f9ad198df8e2cf7bfa09a71892fd4
jbi <biehler@adco-engineering-gw.com>
2018-08-03 14:32:36 +0200

    Fixed error in passing arguments to scipys stats functions.

--- pqueens/utils/input_to_random_variable.py
+++ pqueens/utils/input_to_random_variable.py
@@ -35,11 +35,12 @@ def get_distribution_object(description):
     distribution_parameter = list(description["distribution_parameter"])
     distribution = None
     if  description["distribution"] == 'normal':
-        distribution = scipy.stats.norm(distribution_parameter)
+        print(distribution_parameter)
+        distribution = scipy.stats.norm(distribution_parameter[0], distribution_parameter[1])
     elif description["distribution"] == 'lognormal':
-        distribution = scipy.stats.lognorm(distribution_parameter)
+        distribution = scipy.stats.lognorm(distribution_parameter[0], distribution_parameter[1])
     elif description["distribution"] == 'uniform':
-        distribution = scipy.stats.uniform(distribution_parameter)
+        distribution = scipy.stats.uniform(distribution_parameter[0], distribution_parameter[1])
     else:
         raise ValueError("QUEENS can currently only deal with normal, lognormal"
                          " and uniform distributions, please fix your config file")
