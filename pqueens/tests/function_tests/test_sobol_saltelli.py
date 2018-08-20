8c9a99d9898f7e99300042b99f40e388c8fbc304
jbi <biehler@adco-engineering-gw.com>
2018-08-20 14:14:36 +0200

    Added test case for saltelli iterator.

---
+++ pqueens/tests/function_tests/test_sobol_saltelli.py
@@ -0,0 +1,22 @@
+from pqueens.main import main
+import pytest
+import pickle
+
+def test_sobol_saltelli(tmpdir):
+    """ Test case for saltelli iterator """
+    arguments = ['--input=pqueens/tests/function_tests/input_files/sobol_saltelli.json',
+                 '--output='+str(tmpdir)]
+
+    main(arguments)
+    result_file = str(tmpdir)+'/'+'xxx.pickle'
+    with open(result_file, 'rb') as handle:
+        results = pickle.load(handle)
+
+    assert results["sensitivity_incides"]["S1"][0] == pytest.approx(7.33626148e-01)
+    assert results["sensitivity_incides"]["S1"][1] == pytest.approx(1.75758359e-01)
+    assert results["sensitivity_incides"]["S1"][2] == pytest.approx(2.24732283e-02)
+    assert results["sensitivity_incides"]["S1"][3] == pytest.approx(7.58282827e-03)
+    assert results["sensitivity_incides"]["S1"][4] == pytest.approx(-5.69836812e-05)
+    assert results["sensitivity_incides"]["S1"][5] == pytest.approx(2.42598472e-04)
+    assert results["sensitivity_incides"]["S1"][6] == pytest.approx(2.92864865e-04)
+    assert results["sensitivity_incides"]["S1"][7] == pytest.approx(3.68325749e-05)
