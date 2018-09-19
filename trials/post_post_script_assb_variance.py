173c8434fd3d77fcfd4577fc23d27c5d19014a63
jbi <biehler@adco-engineering-gw.com>
2018-09-19 15:17:10 +0200

    Added post_post processing script to get variance of of concentration in ASSB example.

---
+++ trials/post_post_script_assb_variance.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def run(path_to_scalarvalues_csv):
+    """ Get last line of a csv file and return 20th entry """
+
+    # assemble full filename
+    my_file = path_to_scalarvalues_csv+'0.scalarvalues.csv'
+
+    # system call to get last line
+    p1 = subprocess.Popen(["cat", my_file], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["tail", "-1"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+    output, _ = p2.communicate()
+
+    entries = output.decode().strip('\n').split(",")
+    return entries[19]
