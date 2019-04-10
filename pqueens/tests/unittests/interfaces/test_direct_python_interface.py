a5cab53a82ddabd33fde5290952b47f289674780
Sebastian Brandstaeter <brandstaeter@lnm.mw.tum.de>
2019-04-10 11:58:46 +0200

    Add parallel mapping of variables to responses
    Based on the multipocessing Pool, the DirectPythonInterface can map a
    list of variables to the corresponding list of model responses in
    parallel (i.e. multiple processes).
    The user may specify the number of processes (num_workers) of the Pool.
    Default remains non parallel mapping.
    
    Note that the parallel mapping might increase speed only for expensive
    simulator functions.

--- pqueens/tests/unittests/interfaces/test_direct_python_interface.py
+++ pqueens/tests/unittests/interfaces/test_direct_python_interface.py
@@ -5,6 +5,7 @@ Created on April 10th 2019
 @author: Sebastian Brandstaeter
 
 '''
+import multiprocessing
 import numpy as np
 import pytest
 
@@ -48,16 +49,32 @@ def variables(uncertain_parameters):
 
 
 @pytest.fixture(scope='module')
-def direct_python_interface(variables):
-    """ An instance of Variables class. """
+def list_of_variables(variables):
+    """ A list of multiple Variables instances. """
 
-    interface_name = 'test_interface'
-    python_function_name = 'ishigami.py'
-    return DirectPythonInterface(interface_name, python_function_name, variables)
+    return [variables, variables]
+
+
+@pytest.fixture(scope='module')
+def expected_result():
+    """ Expected result of ishigami funciton for [1., 1., 1.]. """
+
+    return np.array([[5.8821320112036846]])
+
+
+@pytest.fixture(scope='module')
+def expected_results(expected_result):
+    """ Expected results corresponding to list_of_variables. """
+
+    return np.concatenate([expected_result, expected_result])
+
 
 @pytest.fixture(scope='module')
 def config(uncertain_parameters):
-    """ Minimal configuration dictionary to create an interface. """
+    """
+    Minimal config dict to create a Direct-Python-Interface.
+    """
+
     config = {}
     config['test_interface'] = {'type':'direct_python_interface',
                                 'main_file':'ishigami.py'}
@@ -67,17 +84,76 @@ def config(uncertain_parameters):
     return config
 
 
-def test_map(variables, direct_python_interface):
-    """ Test mapping from input to response/ output. """
-    ref_vals = np.array([[5.8821320112036846]])
+@pytest.fixture(scope='module')
+def config_parallel(config):
+    """
+    Minimal config dict to create a Direct-Python-Interface
+    with parallel evaluation of multiple forward calls.
+    """
 
-    output = direct_python_interface.map([variables])
-    np.testing.assert_allclose(output["mean"],ref_vals, 1e-09, 1e-09)
+    config['test_interface']['num_workers'] = 2
+
+    return config
+
+
+@pytest.fixture(scope='module')
+def direct_python_interface(variables):
+    """ An instance of Variables class. """
+
+    interface_name = 'test_interface'
+    python_function_name = 'ishigami.py'
+
+    return DirectPythonInterface(interface_name, python_function_name, variables, num_workers=1)
+
+
+@pytest.fixture(scope='module')
+def direct_python_interface_parallel(variables):
+    """ An instance of Variables class. """
+
+    interface_name = 'test_interface'
+    python_function_name = 'ishigami.py'
+
+    return DirectPythonInterface(interface_name, python_function_name, variables, num_workers=2)
+
+
+def test_map(list_of_variables, expected_results, direct_python_interface):
+    """ Test mapping from input to response/ output. """
+
+    output = direct_python_interface.map(list_of_variables)
+
+    assert direct_python_interface.pool is None
+    np.testing.assert_allclose(output["mean"], expected_results)
+
+
+def test_map_parallel(list_of_variables, expected_results, direct_python_interface_parallel):
+    """
+    Test parallel mapping from multiple input vectors to
+    corresponding responses.
+    """
+
+    output = direct_python_interface_parallel.map(list_of_variables)
+
+    assert isinstance(direct_python_interface_parallel.pool, multiprocessing.pool.Pool)
+    np.testing.assert_allclose(output["mean"], expected_results)
 
 
 def test_create_from_config(variables, config):
     """ Given a config dict instantiate DirectPythonInterface. """
 
-    interface = Interface.from_config_create_interface('test_interface', config)
-    # ensure correct type
-    assert isinstance(interface, DirectPythonInterface)
+    direct_python_interface = Interface.from_config_create_interface('test_interface', config)
+    # ensure correct types
+    assert direct_python_interface.pool is None
+    assert isinstance(direct_python_interface, DirectPythonInterface)
+
+
+def test_create_from_config_parallel(variables, config_parallel):
+    """
+    Given a config dict instantiate DirectPythonInterface
+    with parallel evaluation of multiple forward calls.
+    """
+
+    direct_python_interface_parallel = Interface.from_config_create_interface('test_interface',
+                                                                              config_parallel)
+    # ensure correct types
+    assert isinstance(direct_python_interface_parallel.pool, multiprocessing.pool.Pool)
+    assert isinstance(direct_python_interface_parallel, DirectPythonInterface)
