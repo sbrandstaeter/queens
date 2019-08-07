b52fb5deeb3efce50c3a12d1315ae0bb1500970a
Sebastian Brandstaeter <brandstaeter@lnm.mw.tum.de>
2019-08-07 15:53:05 +0200

    Improve tempering functions, add test (#26)
    The special cases of the tempering are now handled rigorously. Namely
    those are when the temperature (the tempering parameter) is either zero
    or one.
    The reasoning behind this is that in these cases the respective
    distributions are clearly indicated by the temperature.
    A temperature of zero refers to the initial distribution.
    A temperature of one indicates that the goal distribution is reached.
    
    Additionally, I chose to exclude the case of positive infinite logpdfs.
    The reasoning behind is that the results of (+inf + -inf) is ambiguous.
    To circumvent the a tedious management of all special cases, the
    unlikely case of logpdf=+inf is excluded.
    Note that logpdf=-inf on the other hand is very likely. Consider for
    example the uniform distribution is zero (log(0)=-inf) in many areas.
    
    Added unittests for all tempering functionalities.

--- pqueens/utils/smc_utils.py
+++ pqueens/utils/smc_utils.py
@@ -2,28 +2,76 @@
 collection of utility functions and classes for Sequential Monte Carlo
 (SMC) algorithms.
 """
+import math
 
 import numpy as np
+
 
 def temper_logpdf_bayes(log_prior, log_like, tempering_parameter=1.0):
     """
     Bayesian tempering function.
 
     It phases from the prior to the posterior = like * prior.
-    """
-    # the nan_to_num is necessary for the case when tempering_parameter = 0.0 and logpdf=-inf
-    # this would normally return nan but we want it to be 0
-    return tempering_parameter * np.nan_to_num(log_like) + log_prior
+    Special cases are:
+    tempering parameter = 0.0:
+        We interpret this as "disregard contribution of the likelihood".
+        Therefore, return just log_prior.
+
+    log_pior or log_like = +inf:
+        Prohibit this case.
+        The reasoning is that (+inf + -inf) is ambiguous.
+        We know that -inf is likely to occur, e.g., in uniform priors.
+        On the other hand, +inf is rather unlikely to be a reasonable
+        value. Therefore, we chose to exclude it here.
+     """
+    # if either logpdf is positive infinite throw an error
+    if np.isposinf(log_prior).any() or np.isposinf(log_like).any():
+        raise ValueError("Positive infinite logpdf not supported.")
+
+    # if the tempering_parameter is close to 0.0 return prior
+    if math.isclose(tempering_parameter, 0.0, abs_tol=1e-8):
+        return log_prior
+
+    return tempering_parameter * log_like + log_prior
+
 
 def temper_logpdf_generic(logpdf0, logpdf1, tempering_parameter=1.0):
     """
     Generic tempering function.
 
-    It phases from one distribution to another.
+    It phases from one distribution (pdf0) to another (pdf1).
+    initial distribution: pdf0
+    goal distribution: pdf1
+
+    tempering parameter = 0.0:
+        We interpret this as "disregard contribution of the goal pdf".
+        Therefore, return logpdf0.
+
+    tempering parameter = 1.0:
+        We interpret this as "we are fully transitioned." Therefore,
+        ignore the contribution of the initial distribution.
+        Therefore, return logpdf1.
+
+    logpdf0 or logpdf1 = +inf:
+        Prohibit this case.
+        The reasoning is that (+inf + -inf) is ambiguous.
+        We know that -inf is likely to occur, e.g., in uniform
+        distributions. On the other hand, +inf is rather unlikely to be
+        a reasonable value. Therefore, we chose to exclude it here.
     """
-    # the nan_to_num is necessary for the case when tempering_parameter = 0.0 and logpdf=-inf
-    # this would normally return nan but we want it to be 0
-    return tempering_parameter * np.nan_to_num(logpdf1) + logpdf0 * (1.0 - tempering_parameter)
+    # if either logpdf is positive infinite throw an error
+    if np.isposinf(logpdf0).any() or np.isposinf(logpdf1).any():
+        raise ValueError("Positive infinite logpdf not supported.")
+
+    # if the tempering_parameter is close to 0.0 return initial logpdf
+    if math.isclose(tempering_parameter, 0.0, abs_tol=1e-8):
+        return logpdf0
+
+    # if the tempering_parameter is close to 1.0 return final logpdf
+    if math.isclose(tempering_parameter, 1.0):
+        return logpdf1
+
+    return tempering_parameter * logpdf1 + logpdf0 * (1.0 - tempering_parameter)
 
 
 def temper_factory(temper_type):
@@ -35,9 +83,10 @@ def temper_factory(temper_type):
 
     if temper_type == 'bayes':
         return temper_logpdf_bayes
-    elif temper_type == 'generic':
+    if temper_type == 'generic':
         return temper_logpdf_generic
-    else:
-        valid_types = {'bayes', 'generic'}
-        raise ValueError(f"Unknown type of tempering function: {temper_type}."
-                         f"\nValid choices are {valid_types}.")
+
+    valid_types = {'bayes', 'generic'}
+    raise ValueError(
+        f"Unknown type of tempering function: {temper_type}.\nValid choices are {valid_types}."
+    )
