893ede92bd726c35de56b4a421622fcdad0ab88f
Harald Willmann <willmann@lnm.mw.tum.de>
2020-04-30 18:37:16 +0200

    Write csv file
    Add plot_results response with plotly
    
    Include actual variable names in plot
    
    Documentation and Formatting
    
    
    Former-commit-id: d60649fa1424bbf0638cfd2e64def1a4844af461

--- pqueens/iterators/baci_lm_iterator.py
+++ pqueens/iterators/baci_lm_iterator.py
@@ -1,32 +1,22 @@
-"""
-Deterministic optimization
-
-Levenberg Marquardt iterator in the style of baci "gen_inv_analysis"
-
-parts of this class are boldly stolen from optimization_iterator
-we need to take control in the details, although it is less flexible it is far more simple
-
-"""
-import time
-
 import numpy as np
+import os
+import pandas as pd
+import plotly.express as px
 
 from pqueens.iterators.iterator import Iterator
 from pqueens.models.model import Model
 from pqueens.utils.fd_jacobian import fd_jacobian
-from pqueens.utils.process_outputs import write_results
 
 
 class BaciLMIterator(Iterator):
     """
     Iterator for deterministic optimization problems
 
-    Attributes:
-        initial_guess (np.array): initial guess, i.e. start point of
-                                  optimization
-        max_feval (int) : maximal number of Levenberg-Marquardt iterations
-        result_description (dict):  Description of desired
-                                    post-processing
+    Levenberg Marquardt iterator in the style of baci "gen_inv_analysis"
+
+    parts of this class are boldly stolen from optimization_iterator
+    we need to take control in the details, although it is less flexible it is far more simple
+
     """
 
     def __init__(
@@ -47,7 +37,6 @@ class BaciLMIterator(Iterator):
 
         self.initial_guess = initial_guess
         self.param_current = initial_guess
-        self.param_old = initial_guess
         self.jac_rel_step = jac_rel_step
         self.max_feval = max_feval
         self.result_description = result_description
@@ -64,7 +53,7 @@ class BaciLMIterator(Iterator):
     @classmethod
     def from_config_create_iterator(cls, config, iterator_name=None, model=None):
         """
-        Create Optimization iterator from problem description
+        Create Levenberg Marquardt iterator from problem description
 
         Args:
             config (dict): Dictionary with QUEENS problem description
@@ -129,6 +118,12 @@ class BaciLMIterator(Iterator):
     def residual(self, x0):
         """
         Evaluate objective function at x0.
+
+        Args:
+            x0 (numpy.ndarray): vector with current parameters
+
+        Returns:
+            f0 (numpy.ndarray): current residual vector
         """
 
         x_batch, _ = self.get_positions_raw_2pointperturb(x0)
@@ -152,6 +147,12 @@ class BaciLMIterator(Iterator):
         """
         Evaluate Jacobian of objective function at x0.
         For BACI LM we can restrict to "2-point"
+
+        Args:
+            x0 (numpy.ndarray): vector with current parameters
+
+        Returns:
+            J (numpy.ndarray): Jacobian Matrix approximation from finite differences
         """
 
         positions, delta_positions = self.get_positions_raw_2pointperturb(x0)
@@ -183,14 +184,45 @@ class BaciLMIterator(Iterator):
         return J
 
     def initialize_run(self):
-        """ Get initial guess. """
+        """
+        Print console output and optionally open .csv file for results and write header.
+
+        Args:
+            none
+
+        Returns:
+            none
+        """
 
         print("Initialize BACI Levenberg-Marquardt run.")
 
+        # produce .csv file and write header
+        if self.result_description:
+            if self.result_description["write_results"]:
+                df = pd.DataFrame(
+                    columns=['iter', 'resnorm', 'gradnorm', 'params', 'delta_params', 'mu'],
+                )
+                f = open(
+                    os.path.join(
+                        self.global_settings["output_dir"], self.global_settings["experiment_name"]
+                    )
+                    + '.csv',
+                    'w',
+                )
+                df.to_csv(f, sep='\t', index=None)
+                f.close()
+
     def core_run(self):
         """
-        Core run of Optimization iterator
+        Core run of Levenberg Marquardt iterator
+
+        Args:
+            none
+
+        Returns:
+            none
         """
+
         resnorm = float
         resnorm_o = float
 
@@ -201,15 +233,18 @@ class BaciLMIterator(Iterator):
 
         i = 0
 
+        # Levenberg Marquardt iterations
         while not converged:
 
             if i > self.max_feval:
                 converged = True
+                print(f'Maximum number of steps max_feval= {self.max_feval} reached.')
                 break
 
             J = self.jacobian(self.param_current)
             r = self.residual(self.param_current)
 
+            # store terms for repeated useage
             JTJ = (J.T).dot(J)
             JTr = (J.T).dot(r)
 
@@ -224,22 +259,21 @@ class BaciLMIterator(Iterator):
                 gradnorm_o = gradnorm
                 self.lowesterror = resnorm
 
-            #save most optimal step in resnorm
+            # save most optimal step in resnorm
             if resnorm < self.lowesterror:
                 self.lowesterror = resnorm
                 self.param_opt = self.param_current
                 self.iter_opt = i
 
-            # compute update step
+            # compute update step. Not necessary for last iteration! Should usually be low
+            # dimension matrix inversion. We need J and r anyway for convergence check.
             param_delta = -(np.linalg.inv(JTJ + self.reg_param * np.diag(np.diag(JTJ))).dot(JTr))
 
-            # update param for next step
-            self.param_old = self.param_current
-            self.param_current += param_delta
+            # output for iteration. Before update including next step
+            self.printstep(i, resnorm, gradnorm, param_delta)
 
-            print(
-                f"iteration: {i} reg_param: {self.reg_param} current_parameters: {self.param_current}"
-            )
+            # update param for next step
+            self.param_current = self.param_current + param_delta
 
             # update reg_param and check for tolerance
             if self.update_reg == 'res':
@@ -259,22 +293,84 @@ class BaciLMIterator(Iterator):
                 raise ValueError('update_reg unknown')
             i += 1
 
+        # store set of parameters which leads to lowest residual as solution
         self.solution = self.param_opt
 
     def post_run(self):
-        """ Analyze the resulting optimum. """
+        """
+        Write solution to console and optionally create .html plot from result file.
+
+        Args:
+        none
+
+        Returns:
+        none
+        """
 
         print(f"The optimum:\t{self.solution} occured in iteration #{self.iter_opt}.")
-
         if self.result_description:
-            if self.result_description["write_results"]:
-                write_results(
-                    self.solution,
-                    self.global_settings["output_dir"],
-                    self.global_settings["experiment_name"],
+            if self.result_description["plot_results"]:
+                data = pd.read_csv(
+                    os.path.join(
+                        self.global_settings["output_dir"], self.global_settings["experiment_name"]
+                    )
+                    + '.csv',
+                    sep='\t',
                 )
+                xydata = data['params']
+                xydata = xydata.str.extractall('([+-]?\d+\.\d*e?[+-]?\d*)')
+                xydata = xydata.unstack()
+                data = data.drop(columns='params')
+                i = 0
+                var_name = [*self.model.variables[0].variables.keys()]
+                for column in xydata:
+                    data[var_name[i]] = xydata[column]
+                    i = i + 1
+
+                if i > 2:
+                    print(
+                        f'write_results for more than 2 parameters not implemented, '
+                        f'because we are limited to 3 dimensions. '
+                        f'You have: {i}.'
+                    )
+                    pass
+
+                fig = px.line_3d(
+                    data,
+                    x=var_name[0],
+                    y=var_name[1],
+                    z='resnorm',
+                    hover_data=[
+                        'iter',
+                        'resnorm',
+                        'gradnorm',
+                        'delta_params',
+                        'mu',
+                        var_name[0],
+                        var_name[1],
+                    ],
+                )
+                fig.update_traces(mode='lines+markers', marker=dict(size=2), line=dict(width=4))
+                fig.write_html(
+                    os.path.join(
+                        self.global_settings["output_dir"], self.global_settings["experiment_name"]
+                    )
+                    + '.html'
+                )
+        pass
 
     def get_positions_raw_2pointperturb(self, x0):
+
+        """
+        Get parameter sets for objective function evaluations.
+
+        Args:
+        x0 (numpy.ndarray): vector with current parameters
+
+        Returns:
+        positions (numpy.ndarray): parameter batch for function evluation
+        delta_positions (numpy.array): parameter perturbations for finite difference scheme
+        """
 
         delta_positions = self.jac_abs_step + self.jac_rel_step * x0
 
@@ -283,8 +379,51 @@ class BaciLMIterator(Iterator):
 
         for i in range(x0.size):
             positions[i + 1] = x0
-            positions[i + 1][i] += delta_positions[i]
+            positions[i + 1][i] = positions[i + 1][i] + delta_positions[i]
 
         delta_positions.shape = (delta_positions.size, 1)
 
         return positions, delta_positions
+
+    def printstep(self, i, resnorm, gradnorm, param_delta):
+
+        """
+        Print iteration data to console and optionally to file.
+        Opens file in append mode, so that file is updated frequently
+
+        Args:
+        i (int): iteration number
+        resnorm (float): residual norm
+        gradnorm (float): gradient norm
+        param_delta (numpy.ndarray): parameter step
+
+        Returns:
+        None
+        """
+        print(
+            f"iteration: {i} reg_param: {self.reg_param} current_parameters: {self.param_current}"
+        )
+
+        # write iteration to file
+        if self.result_description:
+            if self.result_description["write_results"]:
+                f = open(
+                    os.path.join(
+                        self.global_settings["output_dir"], self.global_settings["experiment_name"]
+                    )
+                    + '.csv',
+                    'a',
+                )
+                df = pd.DataFrame(
+                    {
+                        'iter': i,
+                        'resnorm': resnorm,
+                        'gradnorm': gradnorm,
+                        'params': [self.param_current],
+                        'delta_params': [param_delta],
+                        'mu': self.reg_param,
+                    }
+                )
+                df.to_csv(f, sep='\t', header=None, mode='a', index=None, float_format='%.6f')
+                f.close()
+        pass
