e5080fd67aff52960f931cee2222e7b9ec9794a6
Jonas Nitzler <nitzler@lnm.mw.tum.de>
2020-07-22 15:08:39 +0200

    New folder structure and softlinks

--- pqueens/tests/integration_tests/conftest.py
+++ pqueens/tests/integration_tests/conftest.py
@@ -8,5 +8,5 @@ import pytest
 def inputdir():
     ''' Return the path to the json input-files of the function test. '''
     dirpath = os.path.dirname(__file__)
-    input_files_path = os.path.join(dirpath, 'input_files')
+    input_files_path = os.path.join(dirpath, 'queens_input_files')
     return input_files_path
