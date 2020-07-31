92abf06c1b9886086775945f4dbdd9ebe638e6ab
Barbara Wirthl <barbara.wirthl@tum.de>
2020-07-31 14:56:19 +0200

    Add integration tests for restart #130

--- pqueens/tests/integration_tests/integration_tests_baci/test_baci_sa_morris_salib_invaaa.py
+++ pqueens/tests/integration_tests/integration_tests_baci/test_baci_sa_morris_salib_invaaa.py
@@ -14,12 +14,22 @@ import pytest
 
 
 @pytest.fixture(params=["true", "false"])
-def sing_bool(request):
+def singularity_bool(request):
+    return request.param
+
+
+@pytest.fixture(params=["true", "false"])
+def drop_database_bool(request):
     return request.param
 
 
 def test_baci_morris_salib(
-    inputdir, tmpdir, third_party_inputs, config_dir, set_baci_links_for_gitlab_runner, sing_bool
+    inputdir,
+    tmpdir,
+    third_party_inputs,
+    config_dir,
+    set_baci_links_for_gitlab_runner,
+    singularity_bool,
 ):
     """
     Integration test for the Salib Morris Iterator together with BACI. The test runs a local native
@@ -32,7 +42,7 @@ def test_baci_morris_salib(
         config_dir (str): Path to the config directory of QUEENS containing BACI executables
         set_baci_links_for_gitlab_runner (str): Several paths that are needed to build symbolic
                                                 links to executables
-        sing_bool (str): String that encodes a boolean that is parsed to the JSON input file
+        singularity_bool (str): String that encodes a boolean that is parsed to the JSON input file
 
     Returns:
         None
@@ -69,13 +79,13 @@ def test_baci_morris_salib(
         'baci_input': third_party_input_file,
         'baci-release': baci_release,
         'post_drt_monitor': post_drt_monitor,
-        'singularity_boolean': sing_bool,
+        'singularity_boolean': singularity_bool,
     }
 
     injector.inject(dir_dict, template, input_file)
     arguments = ['--input=' + input_file, '--output=' + str(tmpdir)]
 
-    if sing_bool == 'false':
+    if singularity_bool == 'false':
         # check existence of local singularity image
         script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
         rel_path = '../../../../driver.simg'
@@ -120,6 +130,154 @@ def test_baci_morris_salib(
             "../singularity_recipe\n"
             "----------------------------------------------------\n"
         )
+
+    main(arguments)
+
+    result_file = os.path.join(tmpdir, 'ee_invaaa_local.pickle')
+    with open(result_file, 'rb') as handle:
+        results = pickle.load(handle)
+
+    # test results of SA analysis
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["mu"], np.array([-1.361395, 0.836351]), rtol=1.0e-3,
+    )
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["mu_star"], np.array([1.361395, 0.836351]), rtol=1.0e-3,
+    )
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["sigma"], np.array([0.198629, 0.198629]), rtol=1.0e-3,
+    )
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["mu_star_conf"], np.array([0.11853, 0.146817]), rtol=1.0e-3,
+    )
+
+
+def test_restart_baci(
+    inputdir,
+    tmpdir,
+    third_party_inputs,
+    config_dir,
+    set_baci_links_for_gitlab_runner,
+    singularity_bool,
+    drop_database_bool,
+):
+    """
+    Integration test for the restart functionality (with and without singularity):
+    - test restart from results in database (with drop_existing = false)
+    - test restart from results in output folders (with drop_existing = true).
+
+    Args:
+        inputdir (str): Path to the JSON input file
+        tmpdir (str): Temporary directory in which the pytests are run
+        third_party_inputs (str): Path to the BACI input files
+        config_dir (str): Path to the config directory of QUEENS containing BACI executables
+        set_baci_links_for_gitlab_runner (str): Several paths that are needed to build symbolic
+                                                links to executables
+        singularity_bool (str): String that encodes a boolean that is parsed to the JSON input file
+        drop_database_bool (str): String that encodes a boolean that is parsed to the input file
+
+    Returns:
+        None
+
+    """
+    # Restart based on results of previous tests "test_baci_morris_salib"
+    output_dir_rel = "../test_baci_morris_salib_" + singularity_bool + "_0"
+    output_dir_full = os.path.join(tmpdir, output_dir_rel)
+    output_dir = os.path.abspath(output_dir_full)
+
+    template = os.path.join(inputdir, "morris_baci_local_invaaa_restart_template.json")
+    input_file = os.path.join(tmpdir, "morris_baci_local_invaaa_restart.json")
+    third_party_input_file = os.path.join(third_party_inputs, "baci_input_files", "invaaa_ee.dat")
+
+    baci_release = os.path.join(config_dir, "baci-release")
+    post_drt_monitor = os.path.join(config_dir, "post_drt_monitor")
+
+    dir_dict = {
+        'experiment_dir': str(output_dir),
+        'baci_input': third_party_input_file,
+        'baci-release': baci_release,
+        'post_drt_monitor': post_drt_monitor,
+        'singularity_boolean': singularity_bool,
+        'drop_database_boolean': drop_database_bool,
+    }
+
+    injector.inject(dir_dict, template, input_file)
+    arguments = ['--input=' + input_file, '--output=' + str(tmpdir)]
+
+    main(arguments)
+
+    result_file = os.path.join(tmpdir, 'ee_invaaa_local.pickle')
+    with open(result_file, 'rb') as handle:
+        results = pickle.load(handle)
+
+    # test results of SA analysis
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["mu"], np.array([-1.361395, 0.836351]), rtol=1.0e-3,
+    )
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["mu_star"], np.array([1.361395, 0.836351]), rtol=1.0e-3,
+    )
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["sigma"], np.array([0.198629, 0.198629]), rtol=1.0e-3,
+    )
+    np.testing.assert_allclose(
+        results["sensitivity_indices"]["mu_star_conf"], np.array([0.11853, 0.146817]), rtol=1.0e-3,
+    )
+
+
+def test_block_restart_baci(
+    inputdir,
+    tmpdir,
+    third_party_inputs,
+    config_dir,
+    set_baci_links_for_gitlab_runner,
+    singularity_bool,
+):
+    """
+    Integration test for the block-restart functionality: Delete last results and block-restart
+    those results (with and without singularity).
+
+    Args:
+        inputdir (str): Path to the JSON input file
+        tmpdir (str): Temporary directory in which the pytests are run
+        third_party_inputs (str): Path to the BACI input files
+        config_dir (str): Path to the config directory of QUEENS containing BACI executables
+        set_baci_links_for_gitlab_runner (str): Several paths that are needed to build symbolic
+                                                links to executables
+        singularity_bool (str): String that encodes a boolean that is parsed to the JSON input file
+
+    Returns:
+        None
+
+    """
+    # Restart based on results of previous tests "test_baci_morris_salib"
+    output_dir_rel = "../test_baci_morris_salib_" + singularity_bool + "_0"
+    output_dir_full = os.path.join(tmpdir, output_dir_rel)
+    output_dir = os.path.abspath(output_dir_full)
+
+    # Delete last results
+    for jobid in range(10, 16):
+        rm_cmd = "rm -r " + output_dir + "/" + str(jobid)
+        _, _, _, stderr = run_subprocess(rm_cmd)
+
+    template = os.path.join(inputdir, "morris_baci_local_invaaa_restart_template.json")
+    input_file = os.path.join(tmpdir, "morris_baci_local_invaaa_restart.json")
+    third_party_input_file = os.path.join(third_party_inputs, "baci_input_files", "invaaa_ee.dat")
+
+    baci_release = os.path.join(config_dir, "baci-release")
+    post_drt_monitor = os.path.join(config_dir, "post_drt_monitor")
+
+    dir_dict = {
+        'experiment_dir': str(output_dir),
+        'baci_input': third_party_input_file,
+        'baci-release': baci_release,
+        'post_drt_monitor': post_drt_monitor,
+        'singularity_boolean': singularity_bool,
+        'drop_database_boolean': "true",
+    }
+
+    injector.inject(dir_dict, template, input_file)
+    arguments = ['--input=' + input_file, '--output=' + str(tmpdir)]
 
     main(arguments)
 
