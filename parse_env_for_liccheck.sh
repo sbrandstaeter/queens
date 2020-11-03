1bec04f1a3ddf773f819e4155008ae56c5f65354
Jonas Biehler <biehler@ebenbuild.com>
2020-11-03 15:37:44 +0100

    try to make liccheck happy

--- parse_env_for_liccheck.sh
+++ parse_env_for_liccheck.sh
@@ -6,3 +6,4 @@ sed -i  '/conda-forge/d' requirements.tmp
 sed -i  '/python/d' requirements.tmp
 sed -i  '/cython/d' requirements.tmp
 sed -i  '/pip/d' requirements.tmp
+sed -i  '/pytest/d' requirements.tmp
