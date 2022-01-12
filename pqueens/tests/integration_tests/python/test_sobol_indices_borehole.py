ab22ec62b5431410bf145ba12469b56153e0df52
Barbara Wirthl <barbara.wirthl@tum.de>
2022-01-12 14:25:26 +0100

    feat: update Sobol index estimator
    - use logging
    - clean up print_results method
    - change number of samples to power of two
    - change number of skipped samples to power of two

--- pqueens/tests/integration_tests/python/test_sobol_indices_borehole.py
+++ pqueens/tests/integration_tests/python/test_sobol_indices_borehole.py
@@ -1,3 +1,4 @@
+"""Test Sobol indices estimation for borehole function."""
 import os
 import pickle
 
@@ -19,11 +20,11 @@ def test_sobol_indices_borehole(inputdir, tmpdir):
     with open(result_file, 'rb') as handle:
         results = pickle.load(handle)
 
-    assert results["sensitivity_indices"]["S1"][0] == pytest.approx(8.24597209e-01)
-    assert results["sensitivity_indices"]["S1"][1] == pytest.approx(6.09543158e-05)
-    assert results["sensitivity_indices"]["S1"][2] == pytest.approx(1.48009577e-08)
-    assert results["sensitivity_indices"]["S1"][3] == pytest.approx(4.04630186e-02)
-    assert results["sensitivity_indices"]["S1"][4] == pytest.approx(-3.59511283e-05)
-    assert results["sensitivity_indices"]["S1"][5] == pytest.approx(4.07853843e-02)
-    assert results["sensitivity_indices"]["S1"][6] == pytest.approx(4.02116044e-02)
-    assert results["sensitivity_indices"]["S1"][7] == pytest.approx(8.59606495e-03)
+    assert results["sensitivity_indices"]["S1"][0] == pytest.approx(0.8275788005095177)
+    assert results["sensitivity_indices"]["S1"][1] == pytest.approx(3.626326582692376e-05)
+    assert results["sensitivity_indices"]["S1"][2] == pytest.approx(1.7993448562887368e-09)
+    assert results["sensitivity_indices"]["S1"][3] == pytest.approx(0.04082350205109995)
+    assert results["sensitivity_indices"]["S1"][4] == pytest.approx(-1.0853339811788176e-05)
+    assert results["sensitivity_indices"]["S1"][5] == pytest.approx(0.0427473897346278)
+    assert results["sensitivity_indices"]["S1"][6] == pytest.approx(0.038941629762778956)
+    assert results["sensitivity_indices"]["S1"][7] == pytest.approx(0.009001905983634081)
--- pqueens/tests/integration_tests/python/test_sobol_indices_ishigami.py
+++ pqueens/tests/integration_tests/python/test_sobol_indices_ishigami.py
@@ -1,3 +1,4 @@
+"""Test Sobol indices estimation for Ishigami function."""
 import os
 import pickle
 
@@ -19,26 +20,26 @@ def test_sobol_indices_ishigami(inputdir, tmpdir):
     with open(result_file, 'rb') as handle:
         results = pickle.load(handle)
 
-    assert results["sensitivity_indices"]['S1'][0] == pytest.approx(-0.20343406)
-    assert results["sensitivity_indices"]['S1'][1] == pytest.approx(0.83832789)
-    assert results["sensitivity_indices"]['S1'][2] == pytest.approx(-0.59208696)
+    assert results["sensitivity_indices"]['S1'][0] == pytest.approx(0.12572757495660558)
+    assert results["sensitivity_indices"]['S1'][1] == pytest.approx(0.3888444532476749)
+    assert results["sensitivity_indices"]['S1'][2] == pytest.approx(-0.1701023677236496)
 
-    assert results["sensitivity_indices"]['ST'][0] == pytest.approx(1.34583705)
-    assert results["sensitivity_indices"]['ST'][1] == pytest.approx(0.71692249)
-    assert results["sensitivity_indices"]['ST'][2] == pytest.approx(0.45987242)
+    assert results["sensitivity_indices"]['S1_conf'][0] == pytest.approx(0.3935803586836114)
+    assert results["sensitivity_indices"]['S1_conf'][1] == pytest.approx(0.6623091120357786)
+    assert results["sensitivity_indices"]['S1_conf'][2] == pytest.approx(0.2372589075839736)
 
-    assert results["sensitivity_indices"]['S2'][0, 1] == pytest.approx(0.20866473)
-    assert results["sensitivity_indices"]['S2'][0, 2] == pytest.approx(1.16521165)
-    assert results["sensitivity_indices"]['S2'][1, 2] == pytest.approx(0.2685581)
+    assert results["sensitivity_indices"]['ST'][0] == pytest.approx(0.32520201992825987)
+    assert results["sensitivity_indices"]['ST'][1] == pytest.approx(0.5263552164769918)
+    assert results["sensitivity_indices"]['ST'][2] == pytest.approx(0.1289289258091274)
 
-    assert results["sensitivity_indices"]['S1_conf'][0] == pytest.approx(0.67773088)
-    assert results["sensitivity_indices"]['S1_conf'][1] == pytest.approx(0.60778569)
-    assert results["sensitivity_indices"]['S1_conf'][2] == pytest.approx(0.37909073)
+    assert results["sensitivity_indices"]['ST_conf'][0] == pytest.approx(0.24575185898081872)
+    assert results["sensitivity_indices"]['ST_conf'][1] == pytest.approx(0.5535870474744364)
+    assert results["sensitivity_indices"]['ST_conf'][2] == pytest.approx(0.15792828597131078)
 
-    assert results["sensitivity_indices"]['ST_conf'][0] == pytest.approx(2.21068444)
-    assert results["sensitivity_indices"]['ST_conf'][1] == pytest.approx(0.55758642)
-    assert results["sensitivity_indices"]['ST_conf'][2] == pytest.approx(0.41049134)
+    assert results["sensitivity_indices"]['S2'][0, 1] == pytest.approx(0.6350854922111611)
+    assert results["sensitivity_indices"]['S2'][0, 2] == pytest.approx(1.0749774123116016)
+    assert results["sensitivity_indices"]['S2'][1, 2] == pytest.approx(0.32907368546743065)
 
-    assert results["sensitivity_indices"]['S2_conf'][0, 1] == pytest.approx(1.19398233)
-    assert results["sensitivity_indices"]['S2_conf'][0, 2] == pytest.approx(0.87755398)
-    assert results["sensitivity_indices"]['S2_conf'][1, 2] == pytest.approx(0.76481440)
+    assert results["sensitivity_indices"]['S2_conf'][0, 1] == pytest.approx(0.840605849268133)
+    assert results["sensitivity_indices"]['S2_conf'][0, 2] == pytest.approx(1.2064077218919202)
+    assert results["sensitivity_indices"]['S2_conf'][1, 2] == pytest.approx(0.5803799668636836)
