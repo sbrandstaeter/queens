f9e381cbd37d18eb2f28e19f92784e2905c9c542
Jonas Nitzler <jonas.nitzler@tum.de>
2022-01-24 18:42:23 +0100

    refactor: New post_post base class design
    Refactoring and restructuring of post_post base class
    
    BREAKING CHANGE: Interfaces of post_post class change in need to be adopted

--- pqueens/post_post/post_post.py
+++ pqueens/post_post/post_post.py
@@ -1,213 +1,140 @@
+"""Module for post-post processing of simulation results."""
+
 import abc
-import glob
+import logging
 import os
 
-from pqueens.utils.run_subprocess import run_subprocess
+_logger = logging.getLogger(__name__)
 
 
 class PostPost(metaclass=abc.ABCMeta):
     """Base class for post post processing.
 
     Attributes:
-        delete_data_flag (bool):  Delete files after processing
-        post_post_file_name_prefix_lst (lst): List with prefixes of post-processed files
-        error ():
-        output_dir (str): Path to result files
-        result (np.array): Array containing the quantities of interest
+        post_post_files_regex_lst (lst): List with paths to postprocessed files.
+                                            The file paths can contain regex expression.
+        files_to_be_deleted_regex_lst (lst): List with paths to files that should be deleted.
+                                                The paths can contain regex expressions.
+        driver_name (str): Name of the associated driver.
+        raw_data_lst (lst): Pre-filtered list of raw-data objects from
+                            postprocessed files.
     """
 
-    def __init__(self, delete_data_flag, post_post_file_name_prefix_lst):
+    def __init__(self, post_post_files_regex_lst, files_to_be_deleted_regex_lst, driver_name):
         """Init post post class.
 
         Args:
-            delete_data_flag (bool): Delete files after processing
-            post_post_file_name_prefix_lst (lst): List with prefixes of result files
+            post_post_files_regex_lst (lst): List with paths to postprocessed files.
+                                             The file paths can contain regex expression.
+            files_to_be_deleted_regex_lst (lst): List with paths to files that should be deleted.
+                                                 The paths can contain regex expressions.
+            driver_name (str): Name of the associated driver.
         """
-
-        self.delete_data_flag = delete_data_flag
-        self.post_post_file_name_prefix_lst = post_post_file_name_prefix_lst
-
-        self.error = False
-        self.output_dir = None
-        self.result = None
+        self.post_post_files_regex_lst = post_post_files_regex_lst
+        self.files_to_be_deleted_regex_lst = files_to_be_deleted_regex_lst
+        self.driver_name = driver_name
+        self.raw_data_lst = []
 
     @classmethod
-    def from_config_create_post_post(cls, config, driver_name=None):
+    def from_config_create_post_post(config, driver_name):
         """Create PostPost object from problem description.
 
         Args:
             config (dict): input json file with problem description
-            driver_name (str): Name of driver that should be used in this job-submission
+            driver_name (str): Name of driver that is used in this job-submission
 
         Returns:
-            post_post: post_post object
+            post_post (obj): post_post object
         """
-
         from .post_post_baci import PostPostBACI
-        from .post_post_baci_ensight import PostPostBACIEnsight
-        from .post_post_baci_shape import PostPostBACIShape
-        from .post_post_baci_vectorized import PostPostBACIVector
-        from .post_post_generic import PostPostGeneric
 
         post_post_dict = {
             'baci': PostPostBACI,
-            'baci_vector': PostPostBACIVector,
-            'baci_ensight': PostPostBACIEnsight,
-            'generic': PostPostGeneric,
-            'baci_shape': PostPostBACIShape,
         }
 
-        # determine which object to create
-        # TODO this is not a reliable approach? What if we have multiple drivers?
-        # However, this cannot be fixed by itself here, but we need to
-        # cleanup the whole input parameter handling to fix this.
-        if driver_name:
-            post_post_options = config[driver_name]['driver_params']['post_post']
-        else:
-            post_post_options = config['driver']['driver_params']['post_post']
+        driver_params = config.get(driver_name)
+        if not driver_params:
+            raise ValueError(
+                "No driver parameters found in problem description! "
+                f"You specified the driver name '{driver_name}', "
+                "which could not be found in the problem description. Abort..."
+            )
 
-        post_post_version = post_post_options.get('post_post_approach_sel', None)
-        if post_post_version not in post_post_dict.keys():
-            raise RuntimeError("post_post_approach_sel not set, fix your input file")
+        post_post_options = driver_params.get('post_post')
+        if not post_post_options:
+            raise ValueError(
+                "The 'post_post' options were not found in the driver '{driver_name}'! Abort..."
+            )
+
+        post_post_version = post_post_options.get('post_post_approach_sel')
+        if not post_post_version:
+            raise ValueError(
+                "The posst_post section did not specify a valid 'post_post_approach_sel'! "
+                f"Valid options are {post_post_dict.keys()}. Abort..."
+            )
 
         post_post_class = post_post_dict[post_post_version]
+        post_post = post_post_class.from_config_create_post_post(config, driver_name)
 
-        # ---------------------------- CREATE BASE SETTINGS ---------------------------
-        base_settings = {}
-        base_settings['options'] = post_post_options
-
-        base_settings['config'] = config
-        post_post = post_post_class.from_config_create_post_post(base_settings)
         return post_post
 
-    def copy_post_files(self, files_of_interest, remote_connect, remote_output_dir):
-        """Copy identified post-processed files from "local" output directory
-        to "remote" output directory in case of remote scheduling."""
-
-        remote_file_name = os.path.join(remote_output_dir, '.')
-        command_list = [
-            "scp ",
-            remote_connect,
-            ":",
-            files_of_interest,
-            " ",
-            remote_file_name,
-        ]
-        command_string = ''.join(command_list)
-        _, _, _, stderr = run_subprocess(command_string)
-
-        # detection of failed command
-        if stderr:
-            raise RuntimeError(
-                "\nPost-processed file could not be copied from remote machine!"
-                f"\nStderr:\n{stderr}"
-            )
-
-    def error_handling(self, output_dir):
-        """Mark failed simulation and set results appropriately.
-
-        What does this function do?? This is super unclear
-        """
-        # TODO  ### Error Types ###
-        # No QoI file
-        # Time/Time step not reached
-        # Unexpected values
-
-        # Organized failed files
-        input_file_extention = 'dat'
-        # TODO add documentation what happens here?
-        # TODO Make this platform independent
-        if self.error is True:
-            command_string = (
-                "cd "
-                + output_dir
-                + "&& cd ../.. && mkdir -p postpost_error && cd "
-                + output_dir
-                + r"&& cd .. && mv *."
-                + input_file_extention
-                + r" ../postpost_error/"
-            )
-            _, _, _, _ = run_subprocess(command_string)
-
-    def delete_field_data(self, output_dir, remote_connect):
-        """Delete output files except files with given prefix."""
-
-        inverse_prefix_expr = r"*[!" + self.post_post_file_name_prefix_lst + r"]*"
-        files_of_interest = os.path.join(output_dir, inverse_prefix_expr)
-        post_file_list = glob.glob(files_of_interest)
-
-        for filename in post_file_list:
-            if remote_connect is not None:
-                # generate command for removing 'post-processed file
-                # from output directory on remote machine
-                command_list = [
-                    'ssh',
-                    remote_connect,
-                    '"rm',
-                    filename,
-                    '"',
-                ]
-                command_string = ' '.join(command_list)
-                _, _, _, stderr = run_subprocess(command_string)
-
-                # detection of failed command
-                if stderr:
-                    raise RuntimeError(
-                        "\nPost-processed file could not be removed from remote machine!"
-                        f"\nStderr on remote:\n{stderr}"
-                    )
-            else:
-                os.remove(filename)
-
-    def postpost_main(self, local_output_dir, remote_connect, remote_output_dir):
-        """Main routine for managing post-post-processing.
+    def get_data_from_post_files(self):
+        """Get data of interest from postprocessed files.
 
         Args:
-            local_output_dir (str): Path to "local" output directory
-            remote_connect (str):  address of remote computing resource (only for remote
-            scheduling)
-            remote_output_dir (str): Path to "remote" output directory
-            (from the point of view of the location of the post-processed files)
+            file_paths_lst (list): List with paths to post-processed files.
 
         Returns:
-            result (np.array): Result of the post-post operation which is the current value
-                               of the quantities of interest
+            None
         """
+        for file_path in self.post_post_files_regex_lst:
+            PostPost._check_file_exist(file_path)
+            self._get_raw_data_from_file(file_path)
+            self._check_raw_data()
+            self._manipulate_raw_data()
 
-        # identify post-processed files containing data of interest in "local" output directory
-        prefix_expr = []
-        files_of_interest = []
-        for file_prefix in self.post_post_file_name_prefix_lst:
-            prefix_expr.append('*' + file_prefix + '*')
-            files_of_interest.append(os.path.join(local_output_dir, prefix_expr[-1]))
+        for file_path in self.files_to_be_deleted_regex_lst:
+            PostPost._clean_up(file_path)
 
-        if remote_connect is not None:
-            # copy identified post-processed files from "local" output directory
-            # to "remote" output directory in case of remote scheduling
-            for file in files_of_interest:
-                self.copy_post_files(file, remote_connect, remote_output_dir)
+    @staticmethod
+    def _check_files_exist(file_path):
+        """Check if file exists.
 
-            # set output directory to "remote"
-            output_dir = remote_output_dir
-        else:
-            # set output directory to "local"
-            output_dir = local_output_dir
+        Args:
+            file_path (str): Path to file of interest. The path might contain
+                             regex expressions.
 
-        # get data of interest from identified post-processed files in output directory
-        for current_idx, current_file in enumerate(files_of_interest):
-            self.read_post_files(current_file, idx=current_idx)
-
-        # mark failed simulation and set results appropriately in output directory
-        self.error_handling(output_dir)
-
-        # clear memory by removing all other post-processed files in either "local"
-        # or "remote" output directory
-        if self.delete_data_flag is True:
-            self.delete_field_data(output_dir, remote_connect)
-
-        return self.result
+        Returns:
+            None
+        """
+        file_exists = os.path.isfile(file_path)
+        if not file_exists:
+            raise FileNotFoundError(f"The file '{file_path}' does not exist! Abort...")
 
     @abc.abstractmethod
-    def read_post_files(self, file_names, **kwargs):
-        """This method has to be implemented by all child classes."""
+    def _get_raw_data_from_file(self):
         pass
+
+    @abc.abstractmethod
+    def _check_raw_data(self):
+        pass
+
+    @abc.abstractmethod
+    def _manipulate_raw_data(self):
+        pass
+
+    @staticmethod
+    def _clean_up(file_path):
+        """Clean-up files in the output directory.
+
+        Args:
+            file_path (str): File path with optional regex for the file that
+                             should be deleted.
+
+        Returns:
+            None
+        """
+        try:
+            os.remove(file_path)
+        except (OSError, FileNotFoundError):
+            _logger.debug(f"Could not remove file with path: '{file_path}'. Skip...")
