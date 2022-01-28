f0ed1c48e5853a719d6e9806c880179026f1cebb
Gil Robalo Rei <gil.rei@tum.de>
2022-01-28 18:48:54 +0100

    feat(cli_utils): clean up cli_utils

--- pqueens/utils/cli_utils.py
+++ pqueens/utils/cli_utils.py
@@ -1,22 +1,18 @@
-"""CLI utils collection."""
-import argparse
-
-import pyfiglet
-
+"""Command Line Interface utils collection."""
 from pqueens.utils import ascii_art
 from pqueens.utils.manage_singularity import create_singularity_image
 
 
 def build_singularity_cli():
     """Build singularity image CLI wrapper."""
-    ascii_art.print_crown()
-    print(pyfiglet.figlet_format("SINBUILD", font="banner3-D"))
-    parser = argparse.ArgumentParser(description="QUEENS singularity image builder utility")
+    ascii_art.print_crown(75)
+    ascii_art.print_banner("SINBUILD", 75)
+    print("Singularity image builder for QUEENS runs".center(75))
 
-    print("Building a singularity image! This might take some time")
+    print("\n\nBuilding a singularity image! This might take some time ...")
     try:
         create_singularity_image()
         print("Done!")
     except Exception as cli_singularity_error:
-        print("Building singularity failed!")
+        print("Building singularity failed!\n\n")
         raise cli_singularity_error
