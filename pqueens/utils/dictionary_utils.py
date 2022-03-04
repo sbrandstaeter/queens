05639f3f85b3192d85a027e6ca72be2d56010f22
Gil Robalo Rei <gil.rei@tum.de>
2022-03-04 17:01:15 +0100

    feat(dictionary_utils): add functions to convert dictionaries to named tuples
    Allow for cleaner code

--- pqueens/utils/dictionary_utils.py
+++ pqueens/utils/dictionary_utils.py
@@ -1,3 +1,7 @@
+"""Dictionary utils."""
+from collections import namedtuple
+
+
 def findkeys(dictionary, key):
     """Get all the values of a 'key' in a nested dictionary.
 
@@ -18,3 +22,33 @@ def findkeys(dictionary, key):
         for j in dictionary.values():
             for x in findkeys(j, key):
                 yield x
+
+
+def to_named_tuple(dict_data, tuple_name="named_tuple"):
+    """Create namedtuple from dictionary.
+
+    If the dict contains dicts itself, these remain dicts.
+    Args:
+        dict_data (dict): Dict to be converted to a dict
+        tuple_name (str, optional): Name for the named tuple. Defaults to "named_tuple".
+
+    Returns:
+        namedtuple: Named tuple
+    """
+    return namedtuple(tuple_name, dict_data.keys())(*dict_data.values())
+
+
+def to_named_tuple_nested(dict_data, tuple_name="named_tuple"):
+    """Create namedtuple from dictionary.
+
+    If the dict contains dicts itself, these are also casted into named tuples.
+    Args:
+        dict_data (dict): Dict to be converted to a dict
+        tuple_name (str, optional): Name for the named tuple. Defaults to "named_tuple".
+
+    Returns:
+        namedtuple: Named tuple
+    """
+    return namedtuple(tuple_name, dict_data.keys())(
+        *tuple(map(lambda x: x if not isinstance(x, dict) else toNametuple(x), dict_data.values()))
+    )
