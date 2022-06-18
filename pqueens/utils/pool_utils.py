c6a80b2da36a46d440cac6d08cc7300d2845fbb0
Gil Robalo Rei <gil.rei@tum.de>
2022-06-18 21:57:47 +0200

    feat(pool_utils): add pool utils (pathos multiprocessing)

---
+++ pqueens/utils/pool_utils.py
@@ -0,0 +1,19 @@
+"""Pool utils."""
+from pathos.multiprocessing import ProcessingPool as Pool
+
+
+def create_pool_thread_number(number_of_workers):
+    """Create pathos Pool from number of workers.
+
+    Args:
+        number_of_workers (int): Number of parallel evaluations
+
+    Returns:
+        pathos multiprocessing pool
+    """
+    if isinstance(number_of_workers, int) and number_of_workers > 1:
+        print(f"Activating parallel evaluation of samples with {number_of_workers} workers.\n")
+        pool = Pool(processes=number_of_workers)
+    else:
+        pool = None
+    return pool
