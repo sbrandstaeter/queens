7da5e43e3bb36114027458efb12aa3575c8fc732
Gil Robalo Rei <gil.rei@tum.de>
2022-06-22 18:09:36 +0200

    fix(import-utils): replace load_main_by_path with load_function_by_name_from_path
    To allow function imports not called main

--- pqueens/utils/import_utils.py
+++ pqueens/utils/import_utils.py
@@ -6,20 +6,29 @@ from pathlib import Path
 from pqueens.utils.path_utils import check_if_path_exists
 
 
-def load_main_by_path(path_to_module):
-    """Load a main function from python file by path.
+def load_function_by_name_from_path(path_to_module, function_name):
+    """Load function from python file by path.
 
     Args:
         path_to_module (str): Path to file
+        function_name (str): Name of the function
 
     Returns:
-        (function): Main function of the module
+        (function): function of the module
     """
-    if not check_if_path_exists(path_to_module):
-        raise FileNotFoundError(f"Could not find python file {path_to_module}.")
 
     # Set the module name
-    module_name = Path(path_to_module).stem
+    module_path_obj = Path(path_to_module)
+    module_name = module_path_obj.stem
+    module_ending = module_path_obj.suffix
+
+    # Check if file exsits
+    if not check_if_path_exists(module_path_obj):
+        raise FileNotFoundError(f"Could not find python file {path_to_module}.")
+
+    # Check if ending is correct
+    if module_ending != ".py":
+        raise ImportError(f"Python file {path_to_module} does not have a .py ending")
 
     # Load the module
     spec = importlib.util.spec_from_file_location(module_name, path_to_module)
@@ -27,5 +36,12 @@ def load_main_by_path(path_to_module):
     spec.loader.exec_module(module)
     sys.modules[module_name] = module
 
-    # Return its main function
-    return module.main
+    try:
+        # Check if function can be loaded
+        function = getattr(module, function_name)
+    except AttributeError as error:
+        raise AttributeError(
+            f"External python module {path_to_module} does not have a function {function_name}"
+        ) from error
+
+    return function
