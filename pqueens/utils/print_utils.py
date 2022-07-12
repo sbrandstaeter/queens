3855eff0cdc8df54fbc72e434b9ad185e1ba6d8e
Gil Robalo Rei <gil.rei@tum.de>
2022-07-12 15:41:07 +0200

    feat(print_utils): make get_str_table better
    essential for performance

--- pqueens/utils/print_utils.py
+++ pqueens/utils/print_utils.py
@@ -11,10 +11,26 @@ def get_str_table(name, print_dict):
     Returns:
         str: table to print
     """
-    seperator_line = "-" * 60 + "\n"
+    # lines for the table
+    lines = [name]
+    for key, item in print_dict.items():
+        lines.append(f"  {key}: {item}")
 
-    string = "\n" + seperator_line + name + "\n" + seperator_line
-    for item in print_dict.items():
-        string += f"{item[0]}: {item[1]}\n"
-    string += seperator_line + "\n"
+    # find max width and create sepreators
+    seperator_width = max(max(len(l) for l in lines), 63)
+    if seperator_width % 2 == 0:
+        # odd width just look better
+        seperator_width += 1
+    main_seperator_line = "+" + "-" * seperator_width + "+"
+    soft_separator_line = "|" + "- " * (seperator_width // 2) + "-|"
+
+    # Create table string
+    string = main_seperator_line + "\n"
+    for i, line in enumerate(lines):
+        if i == 0:
+            string += "|" + line.center(seperator_width) + "|\n"
+            string += soft_separator_line + "\n"
+        else:
+            string += "|" + line.ljust(seperator_width) + "|\n"
+    string += main_seperator_line + "\n"
     return string
