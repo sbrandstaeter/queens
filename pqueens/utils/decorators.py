c58bffa7c8ddfa931a32c865f2c5a4f2c4a67f5d
Gil Robalo Rei <gil.rei@tum.de>
2022-07-13 16:52:57 +0200

    fix(decorator_utils): generalize decorator

--- pqueens/utils/decorators.py
+++ pqueens/utils/decorators.py
@@ -1,53 +1,51 @@
+"""Collection of decorators."""
 import logging
+from functools import wraps
+from time import sleep
 
 _logger = logging.getLogger(__name__)
 
-"""Collection of decorators."""
 
-
-def safe_mongodb_operation(fun):
-    """Method decorator in order to process QUEENS MongoDB wrapper methods
-    safely. Do **not** use this decorator on functions with @classmethod
-    decorators! (As we extract the self argument which does not exist for.
-
-    @classmethod functions)
+def safe_operation(function, max_number_of_attempts=10, waiting_time=0.01):
+    """Method decorator in order to process database methods safely.
 
     The safe procedure consists of:
         - Catching and logging exceptions
+        - Waiting `waiting_time`
         - Retrying the method call up to `max_number_of_attempts` times
 
     Args:
-        fun (method): MongoDB method trying to access the database
+        function (function): Function trying to access the database
 
     Returns:
-        wrapper (obj): function that is called instead of `fun`
+        wrapper (obj): function that is called instead of `function`
     """
 
+    @wraps(function)
     def wrapper(*args, **kwargs):
-        """Function in which the method `fun` is wrapped in. In here exceptions
-        are caught and the operation is retried up to
-        `self.max_number_of_attempts` times.
+        """Function in which the method `function` is wrapped in.
+
+        In here exceptions are caught and the operation is retried up to
+        `max_number_of_attempts` times.
 
         Returns:
-            A safe `fun` call.
+            A safe `function` call.
         """
-        # Get the self attribute
-        self = args[0]
-
         # Repeat function call if failed
-        for attempt in range(1, self.max_number_of_attempts + 1):
+        for attempt in range(1, max_number_of_attempts + 1):
             try:
-                return fun(*args, **kwargs)
+                return function(*args, **kwargs)
             except Exception as error:
-                if attempt < self.max_number_of_attempts:
+                function_name = function.__module__ + ":" + function.__name__
+                if attempt < max_number_of_attempts:
                     _logger.warning(
-                        f"Function {fun.__name__}: Attempt number {attempt}/"
-                        f"{self.max_number_of_attempts} to '{self.db_address}' failed!"
+                        f"Function {function_name}: Attempt number {attempt}/"
+                        f"{max_number_of_attempts}"
                     )
+                    sleep(waiting_time)
                 else:
                     raise ValueError(
-                        f"Function {fun.__name__} failed with on '{self.db_address}' after"
-                        f" {self.max_number_of_attempts} attempts!"
+                        f"Function {function_name} failed after {max_number_of_attempts} attempts!"
                     ) from error
 
     return wrapper
