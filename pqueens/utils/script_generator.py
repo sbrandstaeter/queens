46dab5a9740b51f0334fee03ec4aadf26330f9a6
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2022-10-01 03:51:43 +0200

    refactor(utils): use standard library for file copy, built fixed path only once, rename dummy_script

--- pqueens/utils/script_generator.py
+++ pqueens/utils/script_generator.py
@@ -1,12 +1,18 @@
 """Function to generate a job or task script for submission."""
+import shutil
 
 from pqueens.utils.injector import inject
 from pqueens.utils.path_utils import relative_path_from_pqueens
 from pqueens.utils.run_subprocess import run_subprocess
 
+# local dummy path
+LOCAL_TEMPORARY_SUBMISSION_SCRIPT = relative_path_from_pqueens(
+    'utils/temporary_submission_script.sh', as_str=False
+)
+
 
 def generate_submission_script(
-    script_options, submission_script_path, submission_script_template, connect_to_resource=None
+    script_options, submission_script_path, submission_script_template, user_at_host=None
 ):
     """Generate a submission script.
 
@@ -16,19 +22,19 @@ def generate_submission_script(
         script_options (dict): Options for the submission
         submission_script_path (str): Destination path for the script
         submission_script_template (str): Path to submission template
-        connect_to_resource (str, optional): Resource connection string
+        user_at_host (str, optional): Resource connection string in the form of username@hostname
     """
-    # local dummy path
-    local_dummy_path = relative_path_from_pqueens('utils/dummy_submission_script')
-
     # create actual submission file with parsed parameters
-    inject(script_options, submission_script_template, local_dummy_path)
+    inject(script_options, submission_script_template, LOCAL_TEMPORARY_SUBMISSION_SCRIPT)
 
     # copy submission script to specified local or remote location
-    if connect_to_resource is None:
-        command_list = ['cp', local_dummy_path, submission_script_path]
+    if user_at_host is None:
+        shutil.copy(LOCAL_TEMPORARY_SUBMISSION_SCRIPT, submission_script_path)
     else:
-        command_list = ['scp', local_dummy_path, connect_to_resource + ':' + submission_script_path]
-    command_string = ' '.join(command_list)
-
-    run_subprocess(command_string)
+        command_list = [
+            'scp',
+            str(LOCAL_TEMPORARY_SUBMISSION_SCRIPT),
+            user_at_host + ':' + submission_script_path,
+        ]
+        command_string = ' '.join(command_list)
+        run_subprocess(command_string)
