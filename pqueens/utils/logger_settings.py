58b4aad02917ff0c9386a6e949b37dd08faa0eea
Dominik Rabe <d.rabe@unibw.de>
2022-11-02 12:53:39 +0100

    refunctioning of log_through_print by log_multiline_str

--- pqueens/utils/logger_settings.py
+++ pqueens/utils/logger_settings.py
@@ -224,18 +224,3 @@ def finish_job_logger(joblogger, lfh, efh, stream_handler):
     if stream_handler is not None:
         stream_handler.close()
         joblogger.removeHandler(stream_handler)
-
-
-def log_multiline_string(logger, string):
-    """Log multiline string line by line.
-
-    Split a multiline string into lines and uses logger.info to generate output. This might enhance readability
-    as it is in line with formatting of logging.
-
-    Args:
-        logger (Logger): logger to parse string to
-        string (str): string which should be printed
-    """
-
-    for line in string.splitlines():
-        logger.info(line)
