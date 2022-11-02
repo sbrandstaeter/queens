ac67848f0f0f7e7480ee623f9e4d81760961cf66
Dominik Rabe <d.rabe@unibw.de>
2022-11-02 14:28:35 +0100

    refunctioning of log_through_print by log_multiline_str 3

--- pqueens/utils/logger_utils.py
+++ pqueens/utils/logger_utils.py
@@ -1,8 +1,9 @@
+"""Logging in QUEENS"""
 def log_multiline_string(logger, string):
     """Log multiline string line by line.
 
-    Split a multiline string into lines and uses logger.info to generate output. This might enhance readability
-    as it is in line with formatting of logging.
+    Split a multiline string into lines and uses logger.info to generate output. This might
+    enhance readability as it is in line with formatting of logging.
 
     Args:
         logger (Logger): logger to parse string to
