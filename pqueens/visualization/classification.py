29abac6a10d98b30bd52e735cf251719118b84bb
Gil Robalo Rei <gil.rei@tum.de>
2023-01-05 17:49:33 +0100

    refactor(visualization.classification): rename and clean visualization for the classification iterator

---
+++ pqueens/visualization/classification.py
@@ -0,0 +1,286 @@
+"""Classification visualization."""
+import itertools
+import math
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.inspection._plot.decision_boundary
+from matplotlib import cm
+
+
+class ClassificationVisualization:
+    """Visualization class for ClassificationIterator.
+
+    Attributes:
+        saving_path (str): Path to save the plots
+        save_bool (bool): Boolean to save plots
+        plot_boolean (bool): Boolean for determining whether individual plots should be plotted
+
+    Returns:
+        Instance of the visualization class
+    """
+
+    # some overall class states
+    plt.rcParams["mathtext.fontset"] = "cm"
+
+    def __init__(self, saving_path, save_bool, plot_bool):
+        """Initialise iterator.
+
+        Args:
+            saving_path (str): Path to save the plots.
+            save_bool (bool): Boolean to save plots.
+            plot_bool (bool): Boolean for determining whether individual plots should be plotted
+        """
+        self.saving_path = saving_path
+        self.save_bool = save_bool
+        self.plot_bool = plot_bool
+
+    @classmethod
+    def from_config_create(cls, config, iterator_name):
+        """Create the visualization object from the problem description.
+
+        Args:
+            config (dict): Dictionary containing the problem description
+            iterator_name (str): Name of iterator to identify right section in options dict
+
+        Returns:
+            Instance of ClassificationVisualization
+        """
+        method_options = config[iterator_name].get("method_options")
+
+        plotting_options = method_options["result_description"].get("plotting_options")
+        path = os.path.join(plotting_options.get("plotting_dir"), plotting_options["plot_name"])
+        save_bool = plotting_options.get("save_bool")
+        plot_bool = plotting_options.get("plot_bool", False)
+        return cls(saving_path=path, save_bool=save_bool, plot_bool=plot_bool)
+
+    def plot_decision_boundary(
+        self, output, samples, classifier, parameter_names, iteration_index="final"
+    ):
+        """Plot decision boundary of the trained classifier.
+
+        If num_params>2, each combination of 2 parameters is plotted in a separate subplot.
+
+        Args:
+            output (dict): Classification results obtained from simulation
+            samples (np.array): Array with sample points, size: (num_sample_points, num_params)
+            classifier (obj): Classifier object from Queens
+            parameter_names (list): List of parameters names
+            iteration_index (str): additional name for saving plots
+        """
+        num_params = len(parameter_names)
+        all_ind = set(np.arange(num_params))
+        if self.plot_bool or self.save_bool:
+            num_combinations, idx = self._get_axes(num_params)
+            fig, ax = plt.subplots(figsize=(10 * num_combinations, 10), ncols=num_combinations)
+            for i, params in enumerate(idx):
+                axis = ax[i] if num_params > 2 else ax
+                not_params = list(all_ind - set(params))
+                ConditionalDecisionBoundaryDisplay.from_estimator(
+                    classifier.classifier_obj,
+                    samples,
+                    eps=0.1 * np.abs(np.min(samples[:, params])),
+                    params=params,
+                    conditial_values=[
+                        (ii, 0) for ii in not_params
+                    ],  # currently all conditional values are set to 0
+                    ax=axis,
+                    cmap=cm.coolwarm,
+                    alpha=0.8,
+                    xlabel=parameter_names[params[0]],
+                    ylabel=parameter_names[params[1]],
+                    grid_resolution=200,
+                )
+                axis.scatter(
+                    samples[:, params[0]],
+                    samples[:, params[1]],
+                    c=output[:].astype(np.int64),
+                    cmap=cm.coolwarm,
+                    s=20,
+                    edgecolors="k",
+                )
+                title = f'Classification with model calls {len(samples)}'
+                # Plot the samples of the last batch
+                if classifier.is_active:
+                    axis.scatter(
+                        samples[-classifier.batch_size :, params[0]],
+                        samples[-classifier.batch_size :, params[1]],
+                        c="g",
+                        s=100,
+                        marker="x",
+                        label="Last batch",
+                    )
+                    title += f", active learning iteration {iteration_index}"
+                    axis.legend()
+            fig.suptitle(title)
+            self._save_plot(self.save_bool, self.saving_path + f"_{iteration_index}.jpg")
+            plt.close(fig)
+        if self.plot_bool:
+            plt.show()
+
+    def _get_axes(self, num_params):
+        """Get all parameter combinations for conditional plots.
+
+        Args:
+            num_params (int): Number of parameters varied
+
+        Returns:
+            number of combinations, list of parameter indexes
+        """
+        num_combinations = math.comb(num_params, 2)
+        idx = itertools.combinations(range(num_params), 2)
+        return num_combinations, idx
+
+    def _save_plot(self, save_bool, path):
+        """Save the plot to specified path.
+
+        Args:
+            save_bool (bool): Flag to decide whether saving option is triggered.
+            path (str): Path where to save the plot.
+
+        Returns:
+            Saved plot.
+        """
+        if save_bool:
+            plt.savefig(path, dpi=300)
+
+
+def conditional_prediction_decorator(prediction_method, conditial_values):
+    """Decorator for the estimators.
+
+    Currently the DecisionBoundary only creates grids in a 2d setting. Hence the conditional fixed
+    values need to be added.
+
+    Example:
+        4d classifier where the second input parameter is the conditional one and fixed to the
+        valued 5 and the last one to -1.
+
+        conditonal_values=[(1,5),(3,-1)]
+
+    Args:
+        prediction_method (fun): method to be decorated
+        conditial_values (list): list of tuple to add the conditional values.
+
+    Returns:
+        the wrapped method.
+    """
+
+    def predict(*args):
+        args = list(args)
+        # The last argument is X_grid
+        x_grid = args[-1]
+        n_fill = len(x_grid)
+        for parameter in conditial_values:
+            # Add column in parameter[0] with value parameter[1]
+            x_grid = np.insert(x_grid, parameter[0], parameter[1] * np.ones(n_fill), axis=1)
+        args[-1] = x_grid
+        return prediction_method(*args)
+
+    return predict
+
+
+def _check_boundary_response_method(estimator, response_method):
+    """Get the classifier response function.
+
+    We exploit this function to plot the conditional predictions by passing the conditional_values
+    in response_method.
+
+    Args:
+        estimator (obj): Classifier
+        response_method (list): conditional values
+
+    Returns:
+        fun: prediction method
+    """
+    prediction_method = estimator.predict
+
+    if response_method is None:
+        return prediction_method
+
+    return conditional_prediction_decorator(prediction_method, response_method)
+
+
+# Currently sklearn only supports plotting 2 features. This overwrites a sanity check (hacky).
+# pylint: disable=protected-access
+sklearn.inspection._plot.decision_boundary._num_features = lambda X: 2
+sklearn.inspection._plot.decision_boundary._check_boundary_response_method = (
+    _check_boundary_response_method
+)
+DecisionBoundaryDisplay = sklearn.inspection._plot.decision_boundary.DecisionBoundaryDisplay
+# pylint: enable=protected-access
+
+
+class ConditionalDecisionBoundaryDisplay(DecisionBoundaryDisplay):
+    """Custom decision boundary display class."""
+
+    @classmethod
+    def from_estimator(
+        cls,
+        estimator,
+        X,
+        *,
+        params=None,
+        conditial_values=None,
+        grid_resolution=100,
+        eps=1.0,
+        plot_method="contourf",
+        xlabel=None,
+        ylabel=None,
+        ax=None,
+        **kwargs,
+    ):
+        """Create boundary from classifier.
+
+        Example:
+            4d classifier where the second input parameter is the conditional one and fixed to the
+            valued 5 and the last one to -1.
+
+            conditonal_values=[(1,5),(3,-1)]
+            params=(0,2)
+
+        Args:
+            estimator (obj): Trained classifier
+            X (array): Input (n_samples, n_params)
+            params (tuple, optional): List of paramter indices of the input dimensions to
+                                      plot. Defaults to (0, 1).
+            conditial_values (list, optional): List of tuple with the conditional values.
+                                               Defaults to None.
+            grid_resolution (int, optional): Number of grid points to use for plotting decision
+                                             boundary. Higher values will make the plot look nicer
+                                             but be slower to render. Defaults to 100.
+            eps (float, optional): Extends the minimum and maximum values of X for evaluating the
+                                   response function. Defaults to 1.0.
+            plot_method (str, optional):  matplotlib plot methods. Defaults to "contourf".
+            xlabel (str, optional): Label for the x-axis. Defaults to None.
+            ylabel (str, optional): Label for the y-axis. Defaults to None.
+            ax (Matplotlib axes, optional): Axes object to plot on. If `None`, a new figure and
+                                            axes is created. Defaults to None.
+
+        Returns:
+            sklearn.inspection.DecisionBoundaryDisplay object
+        """
+        n_params = X.shape[1]
+        if n_params < 2:
+            raise ValueError("X has to be at least 2d!")
+        if n_params > 2:
+            # If the conditional is to be plotted.
+            # Check if everything is set.
+            if len(params) + len(conditial_values) != n_params:
+                raise ValueError(
+                    "Either the number of params or conditional values is not defined correctly!"
+                )
+
+        # Return the original from_estimator with the modifications in the estimator
+        return super().from_estimator(
+            estimator,
+            X,
+            grid_resolution=grid_resolution,
+            eps=eps,
+            plot_method=plot_method,
+            response_method=conditial_values,
+            xlabel=xlabel,
+            ylabel=ylabel,
+            ax=ax,
+            **kwargs,
+        )
