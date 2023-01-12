88367ae1e69ef19bc9492345621d1dd68de61487
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2023-01-13 00:17:37 +0100

    fix(resources): fix pylint logging-not-lazy and others
    make file pylint compatible except for fixme warning

--- pqueens/resources/resource.py
+++ pqueens/resources/resource.py
@@ -7,7 +7,6 @@ workload capacity. If the workload capacity allows it the computing
 resource accepts jobs and executes them.
 """
 import logging
-import sys
 
 import numpy as np
 
@@ -27,23 +26,20 @@ def parse_resources_from_configuration(config, driver_name):
     Returns:
         dict: Dictionary with resource objects keyed with resource name
     """
-    if "resources" in config:
-        resources = dict()
-        for resource_name, _ in config["resources"].items():
-            global_settings = config.get("global_settings")
-            exp_name = global_settings.get("experiment_name")
-            resources[resource_name] = resource_factory(
-                resource_name, exp_name, config, driver_name
-            )
-        if len(resources.keys()) > 1:
-            raise NotImplementedError(
-                "You indicated more than one resource: {list(config['resources'].keys())}"
-                " Currently QUEENS only supports one!"
-            )
-        return resources
-    # no specified resources
-    else:
+    if "resources" not in config:
         raise Exception("Resources are not properly specified")
+
+    resources = {}
+    for resource_name, _ in config["resources"].items():
+        global_settings = config.get("global_settings")
+        exp_name = global_settings.get("experiment_name")
+        resources[resource_name] = resource_factory(resource_name, exp_name, config, driver_name)
+    if len(resources.keys()) > 1:
+        raise NotImplementedError(
+            "You indicated more than one resource: {list(config['resources'].keys())}"
+            " Currently QUEENS only supports one!"
+        )
+    return resources
 
 
 def resource_factory(resource_name, exp_name, config, driver_name):
@@ -78,7 +74,7 @@ def resource_factory(resource_name, exp_name, config, driver_name):
     return Resource(resource_name, exp_name, scheduler, max_concurrent, max_finished_jobs)
 
 
-class Resource(object):
+class Resource:
     """class which manages a computing resource.
 
     Attributes:
@@ -116,7 +112,7 @@ class Resource(object):
         self.exp_name = exp_name
 
         if len(self.exp_name) == 0:
-            _logger.info("Warning: resource %s has no tasks assigned " " to it" % self.name)
+            _logger.info("Warning: resource %s has no tasks assigned to it", self.name)
 
     def filter_my_jobs(self, jobs):
         """Take a list of jobs and filter those that are on this resource.
@@ -130,8 +126,8 @@ class Resource(object):
         if jobs:
             try:
                 return [job for job in jobs if job['resource'] == self.name]
-            except TypeError:
-                raise TypeError('The design of the jobs list seems wrong! Abort...')
+            except TypeError as exc:
+                raise TypeError('The design of the jobs list seems wrong! Abort...') from exc
         return jobs
 
     def accepting_jobs(self, num_pending_jobs):
@@ -179,15 +175,17 @@ class Resource(object):
         if process_id is not None:
             if process_id != 0:
                 _logger.info(
-                    'Submitted job %d with %s '
-                    '(process id: %d).\n' % (job['id'], self.scheduler_class, process_id)
+                    'Submitted job %d with %s (process id: %d).\n',
+                    job['id'],
+                    self.scheduler_class,
+                    process_id,
                 )
             elif process_id == 0:
                 _logger.info(
-                    'Checked job %d for restart and loaded results into database.\n\n' % job['id']
+                    'Checked job %d for restart and loaded results into database.\n\n', job['id']
                 )
         else:
-            _logger.info('Failed to submit job %d.\n' % job['id'])
+            _logger.info('Failed to submit job %d.\n', job['id'])
 
         return process_id
 
@@ -222,6 +220,4 @@ class Resource(object):
             raise Exception("This job does not belong to me!")
 
         self.scheduler.submit_data_processor(job['id'], batch)
-        _logger.info(
-            'Submitted data processor job %d with %s \n' % (job['id'], self.scheduler_class)
-        )
+        _logger.info('Submitted data processor job %d with %s \n', job['id'], self.scheduler_class)
