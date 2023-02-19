9bbf3e72c1d194a47d4c4a6515b4cd4c7143a11a
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2023-02-19 01:38:50 +0100

    clean up dask interface

--- pqueens/interfaces/dask_interface.py
+++ pqueens/interfaces/dask_interface.py
@@ -5,10 +5,7 @@ import time
 import numpy as np
 from dask.distributed import Client, LocalCluster, as_completed
 
-import pqueens.database.database as DB_module
-from pqueens.database import from_config_create_database
 from pqueens.interfaces.interface import Interface
-from pqueens.resources.resource import parse_resources_from_configuration
 from pqueens.schedulers import from_config_create_scheduler
 from pqueens.utils.config_directories import experiment_directory
 
@@ -25,25 +22,12 @@ class DaskInterface(Interface):
 
     Attributes:
         name (string): Name of interface.
-        resources (dict): Dictionary with resources.
         experiment_name (string): Name of experiment.
-        db (mongodb): MongoDB to store results and job info.
-        polling_time (int): How frequently do we check if jobs are done?
         output_dir (path): Directory to write output to.
         batch_number (int): Number of the current simulation batch.
-        num_pending (int): Number of Jobs that are currently pending.
-        remote (bool): *True* for remote computation.
-        remote_connect (str): Connection to computing resource.
-        scheduler_type (str): Scheduler type.
-        direct_scheduling (bool): *True* if direct scheduling.
-        time_for_data_copy (float): Time (s) to wait such that the copying process of
-                                    simulation input file can finish, and we
-                                    do not overload the network.
         driver_name (str): Name of the associated driver for the current interface.
         _internal_batch_state (int): Helper attribute to compare *batch_number* with the internal
         experiment_dir (path):                       directory to write output to
-        time_for_data_copy (float): Time (s) to wait such that copying process of simulation
-                                    input file can finish, and we do not overload the network
         job_num (int):              Number of the current job
         _internal_batch_state (int): Helper attribute to compare batch_number with the internal
     """
@@ -51,16 +35,8 @@ class DaskInterface(Interface):
     def __init__(
         self,
         interface_name,
-        resources,
         experiment_name,
-        db,
-        polling_time,
         experiment_dir,
-        remote,
-        remote_connect,
-        scheduler_type,
-        direct_scheduling,
-        time_for_data_copy,
         driver_name,
         scheduler,
         dask_scheduler_port,
@@ -69,33 +45,15 @@ class DaskInterface(Interface):
 
         Args:
             interface_name (string):    name of interface
-            resources (dict):           dictionary with resources
             experiment_name (string):   name of experiment
-            db (mongodb):               mongodb to store results and job info
-            polling_time (int):         how frequently do we check if jobs are done
             experiment_dir (path):          directory to write output to
-            remote (bool):              true of remote computation
-            remote_connect (str):       connection to computing resource
-            scheduler_type (str):       scheduler type
-            direct_scheduling (bool):   true if direct scheduling
-            time_for_data_copy (float): Time (s) to wait such that copying process of simulation
-                                        input file can finish, and we do not overload the network
             driver_name (str):          Name of the associated driver for the current interface
         """
         super().__init__(interface_name)
         self.name = interface_name
-        self.resources = resources
         self.experiment_name = experiment_name
-        self.db = db
-        self.polling_time = polling_time
         self.experiment_dir = experiment_dir
         self.batch_number = 0
-        self.num_pending = None
-        self.remote = remote
-        self.remote_connect = remote_connect
-        self.scheduler_type = scheduler_type
-        self.direct_scheduling = direct_scheduling
-        self.time_for_data_copy = time_for_data_copy
         self.driver_name = driver_name
         self._internal_batch_state = 0
         self.job_num = 0
@@ -122,29 +80,29 @@ class DaskInterface(Interface):
         # get experiment name and polling time
         experiment_name = config['global_settings']['experiment_name']
         dask_scheduler_port = config['global_settings']['dask_scheduler_port']
-        polling_time = config.get('polling-time', 1.0)
 
         interface_options = config[interface_name]
         driver_name = interface_options.get('driver_name', None)
         if driver_name is None:
             raise ValueError("No driver_name specified for the JobInterface.")
 
-        # get resources from config
-        resources = parse_resources_from_configuration(config, driver_name)
-
         # get various scheduler options
         # TODO: This is not nice
         first = list(config['resources'])[0]
         scheduler_name = config['resources'][first]['scheduler_name']
         scheduler_options = config[scheduler_name]
-        scheduler_type = scheduler_options['type']
+
+        # create scheduler from config
+        scheduler = from_config_create_scheduler(
+            scheduler_name=scheduler_name,
+            config=config,
+            driver_name=driver_name,
+        )
 
         # get flag for remote scheduling
         if scheduler_options.get('remote'):
-            remote = True
             remote_connect = scheduler_options['remote']['connect']
         else:
-            remote = False
             remote_connect = None
 
         experiment_dir = experiment_directory(
@@ -156,43 +114,11 @@ class DaskInterface(Interface):
         if not isinstance(singularity, bool):
             raise TypeError("Singularity option has to be a boolean (true or false).")
 
-        # set flag for direct scheduling
-        direct_scheduling = False
-        if not singularity:
-            if (scheduler_type == 'cluster') or (scheduler_type == 'standard' and remote):
-                direct_scheduling = True
-
-        db = None
-
-        # get waiting time for copying data
-        time_for_data_copy = interface_options.get('time_for_data_copy')
-
-        resource_name = list(config["resources"].keys())[0]
-        # get resource options extract resource info from config
-        resource_options = config["resources"][resource_name]
-        scheduler_name = resource_options['scheduler_name']
-
-        # create scheduler from config
-        scheduler = from_config_create_scheduler(
-            scheduler_name=scheduler_name,
-            config=config,
-            driver_name=driver_name,
-        )
-        # Create/update singularity image in case of cluster job
-        scheduler.pre_run()
         # instantiate object
         return cls(
             interface_name,
-            resources,
             experiment_name,
-            db,
-            polling_time,
             experiment_dir,
-            remote,
-            remote_connect,
-            scheduler_type,
-            direct_scheduling,
-            time_for_data_copy,
             driver_name,
             scheduler,
             dask_scheduler_port,
@@ -217,7 +143,9 @@ class DaskInterface(Interface):
         jobs = self._manage_jobs(samples)
 
         # get sample and response data
-        output = self.get_output_data(num_samples=samples.shape[0], gradient_bool=gradient_bool, jobs=jobs)
+        output = self.get_output_data(
+            num_samples=samples.shape[0], gradient_bool=gradient_bool, jobs=jobs
+        )
         return output
 
     def create_new_job(self, variables, resource_name, new_id=None):
@@ -322,20 +250,14 @@ class DaskInterface(Interface):
         """
         futures = []
         for jobid in jobid_range:
-            current_job = []
-            if len(current_job) == 1:
-                current_job = current_job[0]
-            elif not current_job:
-                if self._internal_batch_state != self.batch_number:
-                    self._internal_batch_state = self.batch_number
-                    self.job_num = 0
+            if self._internal_batch_state != self.batch_number:
+                self._internal_batch_state = self.batch_number
+                self.job_num = 0
 
-                self.job_num += 1
-                sample_dict = self.parameters.sample_as_dict(samples[self.job_num - 1])
-                resource_name = "DASK"
-                current_job = self.create_new_job(sample_dict, resource_name, jobid)
-            else:
-                raise ValueError(f"Found more than one job with jobid {jobid} in db.")
+            self.job_num += 1
+            sample_dict = self.parameters.sample_as_dict(samples[self.job_num - 1])
+            resource_name = "DASK"
+            current_job = self.create_new_job(sample_dict, resource_name, jobid)
 
             current_job['status'] = 'pending'
 
