fix(tests): fix pymc tests gradient handling
New gradient handling adopted into pymc tests. The patching in the Gaussian likelihood functions doesnt require graident_bool anymore.

pqueens/tests/integration_tests/python/test_gaussian_hmc.py
@@ -24,7 +24,7 @@ def test_gaussian_hmc(inputdir, tmpdir, dummy_data):
     input_file = os.path.join(tmpdir, "gaussian_hmc_realiz.yml")
     injector.inject(dir_dict, template, input_file)
 
-    with patch.object(GaussianLikelihood, "evaluate", target_density):
+    with patch.object(GaussianLikelihood, "evaluate_and_gradient", target_density):
         run(Path(input_file), Path(tmpdir))
 
     result_file = str(tmpdir) + '/' + 'xxx.pickle'
@@ -37,7 +37,7 @@ def test_gaussian_hmc(inputdir, tmpdir, dummy_data):
     assert results['var'].mean(axis=0) == pytest.approx([0, 0])
 
 
-def target_density(self, samples, gradient_bool):
+def target_density(self, samples):
     """Patch likelihood."""
     samples = np.atleast_2d(samples)
     log_likelihood = gaussian_2d_logpdf(samples).flatten()

pqueens/tests/integration_tests/python/test_gaussian_metropolis_hastings_pymc.py
@@ -36,7 +36,7 @@ def test_gaussian_mh(inputdir, tmpdir, dummy_data):
     assert results['var'].mean(axis=0) == pytest.approx([0.13601070852470507, 0.6672200465857734])
 
 
-def target_density(self, samples, gradient_bool=False):
+def target_density(self, samples):
     """Patch likelihood."""
     samples = np.atleast_2d(samples)
     log_likelihood = gaussian_2d_logpdf(samples).flatten()

pqueens/tests/integration_tests/python/test_gaussian_nuts.py
@@ -23,7 +23,7 @@ def test_gaussian_nuts(inputdir, tmpdir, dummy_data):
     dir_dict = {"experimental_data_path": experimental_data_path}
     input_file = os.path.join(tmpdir, "gaussian_nuts_realiz.yml")
     injector.inject(dir_dict, template, input_file)
-    with patch.object(GaussianLikelihood, "evaluate", target_density):
+    with patch.object(GaussianLikelihood, "evaluate_and_gradient", target_density):
         run(Path(input_file), Path(tmpdir))
 
     result_file = str(tmpdir) + '/' + 'xxx.pickle'
@@ -36,7 +36,7 @@ def test_gaussian_nuts(inputdir, tmpdir, dummy_data):
     assert results['var'].mean(axis=0) == pytest.approx([0.08396277217936474, 0.10836256575521087])
 
 
-def target_density(self, samples, gradient_bool):
+def target_density(self, samples):
     """Patch likelihood."""
     samples = np.atleast_2d(samples)
     log_likelihood = gaussian_2d_logpdf(samples).flatten()