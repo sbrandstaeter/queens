fix(iterators): fix gradient handling
Added new gradient handling to PyMC iterators. Metropolis is only calling the forward evals, HMC and NUTS are calling the forward and gradient evaluations.

pqueens/iterators/metropolis_hastings_pymc_iterator.py
@@ -167,11 +167,10 @@ class MetropolisHastingsPyMCIterator(PyMCIterator):
         Returns:
             (np.array): log-likelihoods
         """
-        log_likelihood = np.zeros(shape=(samples.shape[0]))
         # check if sample was seen in previous acceptance step
         if self.seen_samples is None:
             self.seen_samples = [samples.copy(), samples.copy(), samples.copy()]
-            log_likelihood = self.model.evaluate(samples, gradient_bool=False)
+            log_likelihood = self.model.evaluate(samples)
             self.seen_likelihoods = [
                 log_likelihood.copy(),
                 log_likelihood.copy(),
@@ -183,7 +182,7 @@ class MetropolisHastingsPyMCIterator(PyMCIterator):
             elif np.array_equal(self.seen_samples[1], samples):
                 log_likelihood = self.seen_likelihoods[1]
             else:
-                log_likelihood = self.model.evaluate(samples, gradient_bool=False)
+                log_likelihood = self.model.evaluate(samples)
 
         # update list of last samples and likelihoods
         self.seen_samples.pop(0)

pqueens/iterators/nuts_iterator.py
@@ -200,9 +200,9 @@ class NUTSIterator(PyMCIterator):
             log_likelihood = self.current_likelihood
         else:
             self.current_samples = samples.copy()
-            log_likelihood, gradient = self.model.evaluate(samples, gradient_bool=True)
+            log_likelihood, gradient = self.model.evaluate_and_gradient(samples)
             self.current_likelihood = log_likelihood.copy()
-            self.current_gradients = gradient.copy()
+            self.current_gradients = gradient.reshape(-1, self.parameters.num_parameters).copy()
         return log_likelihood
 
     def eval_log_likelihood_grad(self, samples):
@@ -220,8 +220,8 @@ class NUTSIterator(PyMCIterator):
         if np.array_equal(self.current_samples, samples):
             gradient = self.current_gradients
         else:
-            _, gradient = self.model.evaluate(samples, gradient_bool=True)
-        return gradient
+            _, gradient = self.model.evaluate_and_gradient(samples)
+        return gradient.reshape(-1, self.parameters.num_parameters)
 
     def init_mcmc_method(self):
         """Init the PyMC MCMC Model.