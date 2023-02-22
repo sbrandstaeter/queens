91632efa31cc4950d15465d36b36ab5bc4845472
Jonas Nitzler <jonas.nitzler@tum.de>
2023-02-22 12:55:05 +0100

    New Gaussian Neural Network

--- pqueens/tests/integration_tests/example_simulator_functions/sinus.py
+++ pqueens/tests/integration_tests/example_simulator_functions/sinus.py
@@ -5,14 +5,28 @@ import numpy as np
 
 
 def sinus_test_fun(x1, **kwargs):
-    """A standard sine as a test function.
+    """Evaluate standard sine as a test function.
 
     Args:
         x1 (float): Input of the sinus in RAD
 
     Returns:
-        float: Value of the sinus function
+        result (float): Value of the sinus function
     """
     result = np.sin(x1)
-
     return result
+
+
+def gradient_sinus_test_fun(x1, **kwargs):
+    """Evaluate sine and its gradient.
+
+    Args:
+        x1 (float): Input of the sinus in RAD
+
+    Returns:
+        result (float): Value of the sinus function
+        gradient (float): Gradient of the sinus function
+    """
+    result = np.sin(x1)
+    gradient = np.cos(x1)
+    return result, gradient
