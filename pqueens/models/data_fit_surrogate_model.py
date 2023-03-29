fix: gradient in kwargs

pqueens/models/data_fit_surrogate_model.py
@@ -117,11 +117,11 @@ class DataFitSurrogateModel(Model):
             nash_sutcliffe_efficiency,
         )
 
-    def evaluate(self, samples):
+    def evaluate(self, samples, **kwargs):
         """Evaluate model with current set of variables.
 
         Args:
-            samples: TODO_doc
+            samples (np.ndarray): Evaluated samples
 
         Returns:
             np.array: Results corresponding to current set of variables

pqueens/models/finite_difference_model.py
@@ -14,7 +14,6 @@ _logger = logging.getLogger(__name__)
 VALID_FINITE_DIFFERENCE_METHODS = {
     "2-point": "2-point",
     "3-point": "3-point",
-    "cs": "cs",
 }
 
 
@@ -86,7 +85,7 @@ class FiniteDifferenceModel(Model):
         Returns:
             self.response (np.array): Response of the underlying model at current variables
         """
-        if not kwargs['gradient']:
+        if 'gradient' not in kwargs:
             self.response = self.interface.evaluate(samples)
         else:
             self.response = self.evaluate_finite_differences(samples)

pqueens/models/model.py
@@ -1,5 +1,6 @@
 """Model class."""
 import abc
+from typing import final
 
 import numpy as np
 
@@ -20,7 +21,7 @@ class Model(metaclass=abc.ABCMeta):
     Attributes:
         name (str): Name of the model.
         parameters (obj): Parameters object.
-        response (np.array): Response corresponding to parameters.
+        response (dict): Response corresponding to parameters.
     """
 
     def __init__(self, name=None):
@@ -50,6 +51,7 @@ class Model(metaclass=abc.ABCMeta):
             upstream (np.array): Upstream gradient
         """
 
+    @final
     def evaluate_and_gradient(self, samples):
         """Evaluate model output and gradient.
 