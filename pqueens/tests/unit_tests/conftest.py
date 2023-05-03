238dddf4c8f378562e7ff25d4e6c00c593bb9dc9
Dominik Rabe <d.rabe@unibw.de>
2023-05-03 13:23:01 +0200

    fix: enable pylint warning missing-class-docstring

--- pqueens/tests/unit_tests/conftest.py
+++ pqueens/tests/unit_tests/conftest.py
@@ -6,21 +6,6 @@ import pytest
 _logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="session")
-def fake_database():
-    """TODO_doc."""
-
-    class FakeDB(object):
-        """TODO_doc."""
-
-        def print_database_information(self, *args, **kwargs):
-            _logger.info('test')
-
-    # TODO this is super ugly. creation of DB needs te be moved out of
-    # driver init to resolve this
-    return FakeDB()
-
-
 @pytest.fixture(name="test_path")
 def fixture_test_path(tmp_path):
     """Convert *tmp_path* to *pathlib* object."""
