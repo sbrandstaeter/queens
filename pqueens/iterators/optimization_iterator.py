a9c36a7794558d552239243ac2a5e2ba1fa28c4c
Dominik Rabe <d.rabe@unibw.de>
2023-05-17 15:13:48 +0200

    fix: enable pylint warning raise-missing-from

--- pqueens/iterators/optimization_iterator.py
+++ pqueens/iterators/optimization_iterator.py
@@ -386,11 +386,11 @@ class OptimizationIterator(Iterator):
                     )
                     data_list.append(new_experimental_data)
 
-                except FileNotFoundError as e:
-                    raise FileNotFoundError(
-                        "The file containing the high-fidelity Monte-Carlo data was not found!"
-                        " Abort..."
-                    ) from e
+                except IOError as error:
+                    raise IOError(
+                        'An error occurred while reading in the experimental data '
+                        'files. Abort...'
+                    ) from error
             self.experimental_data_dict = pd.concat(data_list, axis=0, ignore_index=True).to_dict(
                 'list'
             )
