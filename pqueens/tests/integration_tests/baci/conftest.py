1bc1841f5c2750a279552ef2a51451d3d0e15949
Gil Robalo Rei <gil.rei@tum.de>
2023-05-26 13:31:26 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- pqueens/tests/integration_tests/baci/conftest.py
+++ pqueens/tests/integration_tests/baci/conftest.py
@@ -99,8 +99,7 @@ def setup_symbolic_links_baci(config_dir, baci_link_paths, baci_source_paths_for
             )
     except FileNotFoundError as error:
         raise FileNotFoundError(
-            f'{error}'
-            + 'Please make sure to make the missing executable available under the given '
+            'Please make sure to make the missing executable available under the given '
             'path OR\n'
             'make sure the symbolic link under the config directory points to an '
             'existing file! \n'
@@ -108,11 +107,12 @@ def setup_symbolic_links_baci(config_dir, baci_link_paths, baci_source_paths_for
             '-------------------------------------------------------------------------\n'
             'ln -s <path/to/baci-release> <QUEENS_BaseDir>/config/baci-release\n'
             'ln -s <path/to/post_drt_monitor> '
-            '<QUEENS_BaseDir>/config/post_drt_monitor\n'
+            '<QUEENS_BaseDir>/config/post_drt_monitor
+'
             'ln -s <path/to/post_processor> <QUEENS_BaseDir>/config/post_processor\n'
             '-------------------------------------------------------------------------\n'
             '...and similar for the other links.'
-        ) from None
+        ) from error
 
 
 @pytest.fixture(params=[True, False])
