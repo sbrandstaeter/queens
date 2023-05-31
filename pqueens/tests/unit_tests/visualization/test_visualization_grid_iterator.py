987e797ac6ec45fe386636035e9b99f4a02c3943
Dominik Rabe <d.rabe@unibw.de>
2023-05-31 14:10:05 +0200

    fix: enable pylint warning assignment-from-no-return

--- pqueens/tests/unit_tests/visualization/test_visualization_grid_iterator.py
+++ pqueens/tests/unit_tests/visualization/test_visualization_grid_iterator.py
@@ -79,7 +79,7 @@ def test_higher_d(dummy_vis):
     """TODO_doc."""
     num_params = 3
     with pytest.raises(NotImplementedError) as e:
-        plotter = qvis.grid_iterator_visualization_instance._get_plotter(num_params)
+        qvis.grid_iterator_visualization_instance._get_plotter(num_params)
     assert str(e.value) == 'Grid plot only possible up to 2 parameters'
 
 
@@ -88,14 +88,14 @@ def test_plot_one_d(dummy_vis):
     output = {'mean': np.array([0.0, 1.0])}
     samples = np.array([0.0, 1.0])
     dummy = 1.0
-    return_value = qvis.grid_iterator_visualization_instance._plot_one_d(output, samples, dummy)
-    assert return_value is None
+    qvis.grid_iterator_visualization_instance._plot_one_d(output, samples, dummy)
+    assert True
 
 
 def test_plot_two_d(dummy_vis):
     """TODO_doc."""
     samples = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
     output = {'mean': np.array([0.0, 1.0, 0.0, 1.0])}
-    n_gird_p = [2, 2]
-    return_value = qvis.grid_iterator_visualization_instance._plot_two_d(output, samples, n_gird_p)
-    assert return_value is None
+    n_grid_p = [2, 2]
+    qvis.grid_iterator_visualization_instance._plot_two_d(output, samples, n_grid_p)
+    assert True
