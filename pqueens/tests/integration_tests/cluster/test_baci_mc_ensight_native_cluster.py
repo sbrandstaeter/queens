50ca6708ad737999821949c56cc7a02e9c70c10e
Gil Robalo Rei <gil.rei@tum.de>
2023-07-17 17:00:40 +0200

    test(native_cluster): switch tests to MC and use the same BACI input file as the local BACI tests

--- pqueens/tests/integration_tests/cluster/test_baci_mc_ensight_native_cluster.py
+++ pqueens/tests/integration_tests/cluster/test_baci_mc_ensight_native_cluster.py
@@ -1,15 +1,13 @@
-"""Test suite for native cluster tests.
-
-Elementary Effects simulations with BACI using the INVAAA minimal model.
-"""
+"""Test suite for native cluster tests."""
 import logging
-from pathlib import Path
 
+import numpy as np
 import pytest
 
 from pqueens import run
 from pqueens.schedulers.cluster_scheduler import BRUTEFORCE_CLUSTER_TYPE, DEEP_CLUSTER_TYPE
 from pqueens.utils import injector
+from pqueens.utils.io_utils import load_result
 
 _logger = logging.getLogger(__name__)
 
@@ -22,13 +20,14 @@ _logger = logging.getLogger(__name__)
     ],
     indirect=True,
 )
-def test_cluster_native_baci_elementary_effects(
+def test_baci_mc_ensight_native_cluster(
     inputdir,
     tmp_path,
     third_party_inputs,
     baci_cluster_paths_native,
     cluster,
-    baci_elementary_effects_check_results,
+    baci_example_expected_mean,
+    baci_example_expected_var,
 ):
     """Test for the Elementary Effects Iterator on the clusters with BACI.
 
@@ -38,26 +37,25 @@ def test_cluster_native_baci_elementary_effects(
         third_party_inputs (Path): Path to the BACI input files
         baci_cluster_paths_native (dict): Paths to baci native on cluster
         cluster (str): Cluster name
-        baci_elementary_effects_check_results (function): function to check the results
+        baci_example_expected_mean (np.ndarray): Expected mean for the MC samples
+        baci_example_expected_var (np.ndarray): Expected var for the MC samples
     """
     path_to_executable = baci_cluster_paths_native["path_to_executable"]
-    path_to_drt_monitor = baci_cluster_paths_native["path_to_drt_monitor"]
+    path_to_post_ensight = baci_cluster_paths_native["path_to_post_ensight"]
 
-    experiment_name = cluster + "_native_elementary_effects"
+    experiment_name = f"baci_mc_ensight_native_{cluster}"
 
-    template = inputdir / "baci_cluster_native_elementary_effects_template.yml"
-    input_file = tmp_path / f"elementary_effects_{cluster}_invaaa.yml"
+    template = inputdir / "baci_mc_ensight_native_cluster_template.yml"
+    input_file = tmp_path / f"baci_mc_ensight_native_cluster_{cluster}.yml"
 
-    baci_input_filename = "invaaa_ee.dat"
-    third_party_input_file_local = (
-        third_party_inputs / "baci_input_files" / f"{baci_input_filename}"
-    )
+    baci_input_filename = "meshtying3D_patch_lin_duallagr_new_struct.dat"
+    third_party_input_file_local = third_party_inputs / "baci_input_files" / baci_input_filename
 
     dir_dict = {
         'experiment_name': experiment_name,
         'input_template': third_party_input_file_local,
         'path_to_executable': path_to_executable,
-        'path_to_drt_monitor': path_to_drt_monitor,
+        'path_to_drt_monitor': path_to_post_ensight,
         'cluster': cluster,
     }
 
@@ -65,4 +63,8 @@ def test_cluster_native_baci_elementary_effects(
     run(input_file, tmp_path)
 
     result_file = tmp_path / f"{experiment_name}.pickle"
-    baci_elementary_effects_check_results(result_file)
+    results = load_result(result_file)
+
+    # assert statements
+    np.testing.assert_array_almost_equal(results['mean'], baci_example_expected_mean, decimal=6)
+    np.testing.assert_array_almost_equal(results['var'], baci_example_expected_var, decimal=6)
