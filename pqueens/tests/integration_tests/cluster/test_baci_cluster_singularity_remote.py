8fdd6c55dc9ed1ad30fa0dfee87313523cbd0de9
Gil Robalo Rei <gil.rei@tum.de>
2023-07-17 17:11:21 +0200

    refactor(baci_cluster_singularity_data_processor_ensight): remove test without iterator
    This is now covered by test the test_baci_mc_ensight_singularity_cluster.

--- pqueens/tests/integration_tests/cluster/test_baci_cluster_singularity_remote.py
+++ pqueens/tests/integration_tests/cluster/test_baci_cluster_singularity_remote.py
@@ -3,14 +3,9 @@
 import logging
 from pathlib import Path
 
-import numpy as np
 import pytest
 
-import pqueens.database.database as DB_module
-import pqueens.parameters.parameters as parameters_module
 from pqueens import run
-from pqueens.main import get_config_dict
-from pqueens.models import from_config_create_model
 from pqueens.schedulers.cluster_scheduler import (
     BRUTEFORCE_CLUSTER_TYPE,
     CHARON_CLUSTER_TYPE,
@@ -183,99 +178,3 @@ class TestClusterSingularity:
 
         result_file = tmp_path / f"{experiment_name}.pickle"
         baci_elementary_effects_check_results(result_file)
-
-    def test_baci_cluster_data_processor_ensight(
-        self,
-        inputdir,
-        tmp_path,
-        cluster_settings,
-        prepare_baci_input_template_remote,
-        baci_cluster_paths,
-        user,
-    ):
-        """Test remote BACI simulations with ensight data-processor.
-
-        Test suite for remote BACI simulations on the cluster in combination
-        with the BACI ensight data-processor. No iterator is used, the model is
-        called directly.
-
-        This integration test is constructed such that:
-            - The interface-map function is called twice (mimics feedback-loops)
-            - The maximum concurrent job is activated
-            - *data_processor_ensight* communicate with the remote database
-            - No iterator is used to reduce complexity
-
-        Args:
-            inputdir (Path): Path to the JSON input file
-            tmp_path (Path): Temporary directory for this test
-            cluster_settings (dict): Collection of cluster specific settings
-            prepare_baci_input_template_remote (fct): helper function to copy template to remote
-            baci_cluster_paths (dict): collection of paths to BACI executables on the cluster
-            user (str): name of the user running the tests
-        """
-        cluster_name = cluster_settings["name"]
-
-        # unique experiment name
-        experiment_name = f"test_{cluster_name}_data_processor_ensight"
-
-        baci_input_template_name = "invaaa_ee.dat"
-        cluster_baci_input_file_template = prepare_baci_input_template_remote(
-            baci_input_template_name=baci_input_template_name, experiment_name=experiment_name
-        )
-
-        template_options = {
-            **baci_cluster_paths,
-            **cluster_settings,
-            'experiment_name': experiment_name,
-            'input_template': cluster_baci_input_file_template,
-            'cluster': cluster_name,
-            'user': user,
-        }
-
-        queens_input_file_template = inputdir / "baci_cluster_data_processor_ensight_template.yml"
-        queens_input_file = tmp_path / f"baci_cluster_data_processor_ensight_{cluster_name}.yml"
-        injector.inject(template_options, queens_input_file_template, queens_input_file)
-
-        # Patch the missing config arguments
-        config = get_config_dict(queens_input_file, tmp_path)
-
-        # Initialise db module
-        DB_module.from_config_create_database(config)
-
-        with DB_module.database as db:  # pylint: disable=no-member
-            # Add experimental coordinates to the database
-            experimental_data_dict = {"x1": [-16, 10], "x2": [7, 15], "x3": [0.63, 0.2]}
-            db.save(experimental_data_dict, experiment_name, 'experimental_data', 1)
-
-            parameters_module.from_config_create_parameters(config)
-
-            # Create a BACI model for the benchmarks
-            model = from_config_create_model("model", config)
-
-            # Evaluate the first batch
-            first_sample_batch = np.array([[0.2, 10], [0.3, 20], [0.45, 100]])
-            first_batch = np.array(model.evaluate(first_sample_batch)["mean"])
-
-            # Evaluate a second batch
-            # In order to make sure that no port is closed after one batch
-            second_sample_batch = np.array([[0.25, 25], [0.4, 46], [0.47, 211]])
-            second_batch = np.array(model.evaluate(second_sample_batch)["mean"][-3:])
-
-        # Check results
-        first_batch_reference_solution = np.array(
-            [
-                [-0.0006949830567464232, 0.0017958658281713724],
-                [-0.0012194387381896377, 0.003230389906093478],
-                [-0.004366828128695488, 0.0129017299041152],
-            ]
-        )
-        second_batch_reference_solution = np.array(
-            [
-                [-0.0016464125365018845, 0.004280212335288525],
-                [-0.0023093123454600573, 0.00646978011354804],
-                [-0.008715332485735416, 0.026327939704060555],
-            ]
-        )
-
-        np.testing.assert_array_equal(first_batch, first_batch_reference_solution)
-        np.testing.assert_array_equal(second_batch, second_batch_reference_solution)
