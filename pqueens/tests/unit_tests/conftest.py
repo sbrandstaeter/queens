114bbbcb3db10eb71fe0574ebf6123798f05e5af
Maximilian Dinkel <maximilian.dinkel@tum.de>
2023-08-06 14:07:02 +0200

    refactor: unit tests fixture

--- pqueens/tests/unit_tests/conftest.py
+++ pqueens/tests/unit_tests/conftest.py
@@ -10,9 +10,5 @@ def dummy_global_settings(tmp_path):
     """Dummy GlobalSettings object."""
     # Setup global settings
     global_settings = GlobalSettings(experiment_name="dummy_experiment_name", output_dir=tmp_path)
-    global_settings.__enter__()  # pylint: disable=unnecessary-dunder-call
-    yield global_settings
-
-    # Tear down global settings
-    print("\ntearing down")
-    global_settings.__exit__(None, None, None)
+    with global_settings:
+        yield global_settings
