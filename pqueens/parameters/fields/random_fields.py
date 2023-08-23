d0a9c72074d87b43e1819f0acbdf6dc3ff9960a8
Dominik Rabe <d.rabe@unibw.de>
2023-08-23 18:12:40 +0200

    fix: bug fix pylint warnings

--- pqueens/parameters/fields/random_fields.py
+++ pqueens/parameters/fields/random_fields.py
@@ -49,19 +49,20 @@ class RandomField:
         self.dimension = len(coords['keys'])
         self.mean_param = mean_param
         self.mean_type = mean_type
-        self.mean = None
+        self.mean = self.calculate_mean_fun()
         self.coords = coords
         self.std_hyperparam_rf = std_hyperparam_rf
         self.corr_length = corr_length
         self.nugget_variance_rf = 1e-9
         self.explained_variance = explained_variance
-        self.initialize()
-        self.dim_truncated = self.weighted_eigen_val_mat_truncated.shape[1]
-        self.K_mat = None
-        self.cholesky_decomp_covar_mat = None
-        self.eigen_vals_vec = None
-        self.eigen_vecs_mat = None
         self.weighted_eigen_val_mat_truncated = None
+        self.K_mat = self.calculate_covariance_matrix_and_cholseky()
+        self.cholesky_decomp_covar_mat = np.linalg.cholesky(self.K_mat)
+        # compute eigendecomposition
+        self.eigen_vals_vec, self.eigen_vecs_mat = self.compute_eigendecomposition()
+        # decompose and truncate the random field
+        self.weighted_eigen_val_mat_truncated = self._decompose_and_truncate_random_field()
+        self.dim_truncated = self.weighted_eigen_val_mat_truncated.shape[1]
 
     def draw(self, num_samples):
         """Draw samples from the truncated representation of the random field.
@@ -95,19 +96,20 @@ class RandomField:
 
         return sample
 
-    def initialize(self):
-        """Calculate discretized mean and covariance of random field."""
-        self.calculate_mean_fun()
-        self.calculate_covariance_matrix_and_cholseky()
-
     def calculate_mean_fun(self):
         """Calculate the discretized mean function."""
         if self.mean_type == 'inflow_parabola':
             fixed_one_dim_coords_vector = np.linspace(0, 1, self.dimension, endpoint=True)
             # Parabola that has its maximum at x = 0
-            self.mean = 4 * self.mean_param * (-((fixed_one_dim_coords_vector - 0.5) ** 2) + 0.25)
+            mean = 4 * self.mean_param * (-((fixed_one_dim_coords_vector - 0.5) ** 2) + 0.25)
         elif self.mean_type == 'constant':
-            self.mean = self.mean_param * np.ones(self.dimension)
+            mean = self.mean_param * np.ones(self.dimension)
+        else:
+            raise ValueError(
+                f"Unknown mean_type. You entered {self.mean_type}, "
+                "but we only accept 'inflow_parabola' and 'constant'."
+            )
+        return mean
 
     def calculate_covariance_matrix_and_cholseky(self):
         """Calculate discretized covariance matrix and cholesky decomposition.
@@ -124,11 +126,15 @@ class RandomField:
                     -(np.linalg.norm(x_one - x_two) ** 2) / (2 * self.corr_length**2)
                 )
 
-        self.K_mat = K_mat + self.nugget_variance_rf * np.eye(self.dimension)
-        self.cholesky_decomp_covar_mat = np.linalg.cholesky(self.K_mat)
+        return K_mat + self.nugget_variance_rf * np.eye(self.dimension)
 
-        # decompose and truncate the random field
-        self._decompose_and_truncate_random_field()
+    def compute_eigendecomposition(self):
+        """Compute eigenvalues and eigenvectors of covariance matrix."""
+        # TODO we should use the information about the Cholesky decomp
+        eig_val, eig_vec = np.linalg.eigh(self.K_mat)
+        eigen_vals_vec = np.real(eig_val)
+        eigen_vecs_mat = np.real(eig_vec)
+        return eigen_vals_vec, eigen_vecs_mat
 
     def _decompose_and_truncate_random_field(self):
         """Decompose and then truncate the random field.
@@ -136,12 +142,6 @@ class RandomField:
         According to desired variance fraction that should be
         covered/explained by the truncation.
         """
-        # compute eigendecomposition
-        # TODO we should use the information about the Cholesky decomp
-        eig_val, eig_vec = np.linalg.eigh(self.K_mat)
-        self.eigen_vals_vec = np.real(eig_val)
-        self.eigen_vecs_mat = np.real(eig_vec)
-
         sum_val = 0
         sum_eigenval = np.sum(self.eigen_vals_vec)
         # calculate m, which is the truncated length and covers 98% of variance
@@ -165,6 +165,4 @@ class RandomField:
         eigen_val_red_diag_mat = np.diagflat(eig_val_vec_red)
 
         # weight the eigenbasis with the eigenvalues
-        self.weighted_eigen_val_mat_truncated = np.dot(
-            eigen_vec_mat_red, np.sqrt(eigen_val_red_diag_mat)
-        )
+        return np.dot(eigen_vec_mat_red, np.sqrt(eigen_val_red_diag_mat))
