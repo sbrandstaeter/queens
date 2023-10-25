6349229628f3a90eeac8501f4584dbf1e84bff8f
Gil Robalo Rei <gil.rei@tum.de>
2023-10-25 15:49:24 +0200

    refactor(surrogate_visualization): remove unused import

--- queens/visualization/surrogate_visualization.py
+++ queens/visualization/surrogate_visualization.py
@@ -19,7 +19,6 @@ from pathlib import Path
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 from matplotlib import style
 from matplotlib.cm import ScalarMappable
 
