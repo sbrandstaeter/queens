d37d291c39d7c2ac263a8803fd3fe50a96bda41e
Dominik Rabe <d.rabe@unibw.de>
2024-04-03 12:38:45 +0200

    fix: switch input file to script test_elementary_effects_sobol.py

--- tests/integration_tests/python/test_elementary_effects_sobol.py
+++ tests/integration_tests/python/test_elementary_effects_sobol.py
@@ -1,9 +1,14 @@
 """TODO_doc."""
-
 import numpy as np
 import pytest
 
-from queens.main import run
+from queens.distributions.uniform import UniformDistribution
+from queens.global_settings import GlobalSettings
+from queens.interfaces.direct_python_interface import DirectPythonInterface
+from queens.iterators.elementary_effects_iterator import ElementaryEffectsIterator
+from queens.main import run_iterator
+from queens.models.simulation_model import SimulationModel
+from queens.parameters.parameters import Parameters
 from queens.utils.input_to_script import create_script_from_input_file
 from queens.utils.io_utils import load_result
 from queens.utils.run_subprocess import run_subprocess
@@ -70,12 +75,59 @@ def fixture_expected_result_sigma():
 
 
 def test_elementary_effects_sobol(
-    inputdir, tmp_path, expected_result_mu, expected_result_mu_star, expected_result_sigma
+    tmp_path, expected_result_mu, expected_result_mu_star, expected_result_sigma
 ):
     """Test case for elementary effects on Sobol's G-function."""
-    run(inputdir / 'elementary_effects_sobol.yml', tmp_path)
+    # Global settings
+    experiment_name = "elementary_effects_sobol"
+    output_dir = tmp_path
 
-    results = load_result(tmp_path / 'xxx.pickle')
+    with GlobalSettings(experiment_name=experiment_name, output_dir=output_dir, debug=False) as gs:
+        # Parameters
+        x1 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x2 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x3 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x4 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x5 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x6 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x7 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x8 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x9 = UniformDistribution(lower_bound=0, upper_bound=1)
+        x10 = UniformDistribution(lower_bound=0, upper_bound=1)
+        parameters = Parameters(
+            x1=x1, x2=x2, x3=x3, x4=x4, x5=x5, x6=x6, x7=x7, x8=x8, x9=x9, x10=x10
+        )
+
+        # Setup QUEENS stuff
+        interface = DirectPythonInterface(function="sobol_g_function", parameters=parameters)
+        model = SimulationModel(interface=interface)
+        iterator = ElementaryEffectsIterator(
+            seed=2,
+            num_trajectories=100,
+            num_optimal_trajectories=4,
+            number_of_levels=10,
+            confidence_level=0.95,
+            local_optimization=False,
+            num_bootstrap_samples=1000,
+            result_description={
+                "write_results": True,
+                "plotting_options": {
+                    "plot_booleans": [False, False],
+                    "plotting_dir": "dummy",
+                    "plot_names": ["bars", "scatter"],
+                    "save_bool": [False, False],
+                },
+            },
+            model=model,
+            parameters=parameters,
+        )
+
+        # Actual analysis
+        run_iterator(iterator)
+
+        # Load results
+        result_file = gs.output_dir / f"{gs.experiment_name}.pickle"
+        results = load_result(result_file)
 
     np.testing.assert_allclose(results["sensitivity_indices"]['mu'], expected_result_mu)
     np.testing.assert_allclose(results["sensitivity_indices"]['mu_star'], expected_result_mu_star)
