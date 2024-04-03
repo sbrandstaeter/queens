32e63111781274c1a732c4cb94526c19ac475a18
Dominik Rabe <d.rabe@unibw.de>
2024-04-03 13:06:16 +0200

    fix: delete test in elementare_effetcs_sobol_from_script

--- tests/integration_tests/python/test_elementary_effects_sobol.py
+++ tests/integration_tests/python/test_elementary_effects_sobol.py
@@ -9,9 +9,7 @@ from queens.iterators.elementary_effects_iterator import ElementaryEffectsIterat
 from queens.main import run_iterator
 from queens.models.simulation_model import SimulationModel
 from queens.parameters.parameters import Parameters
-from queens.utils.input_to_script import create_script_from_input_file
 from queens.utils.io_utils import load_result
-from queens.utils.run_subprocess import run_subprocess
 
 
 @pytest.fixture(name="expected_result_mu")
@@ -128,27 +126,6 @@ def test_elementary_effects_sobol(
         # Load results
         result_file = gs.output_dir / f"{gs.experiment_name}.pickle"
         results = load_result(result_file)
-
-    np.testing.assert_allclose(results["sensitivity_indices"]['mu'], expected_result_mu)
-    np.testing.assert_allclose(results["sensitivity_indices"]['mu_star'], expected_result_mu_star)
-    np.testing.assert_allclose(results["sensitivity_indices"]['sigma'], expected_result_sigma)
-
-
-def test_elementary_effects_sobol_from_script(
-    inputdir, tmp_path, expected_result_mu, expected_result_mu_star, expected_result_sigma
-):
-    """Test case for elementary effects using a script."""
-    input_file = inputdir / 'elementary_effects_sobol.yml'
-    script_path = tmp_path / "script.py"
-    create_script_from_input_file(input_file, tmp_path, script_path)
-
-    # Command to call the script
-    command = f"python {str(script_path.resolve())}"
-
-    # The False is needed due to the loading bars
-    run_subprocess(command, raise_error_on_subprocess_failure=False)
-
-    results = load_result(tmp_path / 'xxx.pickle')
 
     np.testing.assert_allclose(results["sensitivity_indices"]['mu'], expected_result_mu)
     np.testing.assert_allclose(results["sensitivity_indices"]['mu_star'], expected_result_mu_star)
