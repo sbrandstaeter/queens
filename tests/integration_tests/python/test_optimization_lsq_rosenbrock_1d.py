6738ccf77a0d7fa73fb0b3b12be58b7a5f19ac40
Dominik Rabe <d.rabe@unibw.de>
2024-04-10 18:54:05 +0200

    fix: switch input file to script test_optimization_lsq_rosenbrock_1d

--- tests/integration_tests/python/test_optimization_lsq_rosenbrock_1d.py
+++ tests/integration_tests/python/test_optimization_lsq_rosenbrock_1d.py
@@ -4,17 +4,48 @@ import pickle
 
 import numpy as np
 
-from queens.main import run
+from queens.distributions.free import FreeVariable
+from queens.global_settings import GlobalSettings
+from queens.interfaces.direct_python_interface import DirectPythonInterface
+from queens.iterators.optimization_iterator import OptimizationIterator
+from queens.main import run_iterator
+from queens.models.simulation_model import SimulationModel
+from queens.parameters.parameters import Parameters
 
 
-def test_optimization_lsq_rosenbrock_1d(inputdir, tmp_path):
+def test_optimization_lsq_rosenbrock_1d(tmp_path):
     """Test special case for optimization iterator with least squares.
 
     Special case: 1 unknown but 2 residuals.
     """
-    run(inputdir / 'optimization_lsq_rosenbrock_1d.yml', tmp_path)
+    # Global settings
+    experiment_name = "ResRosenbrock1DLSQ"
+    output_dir = tmp_path
 
-    result_file = tmp_path / 'ResRosenbrock1DLSQ.pickle'
+    with GlobalSettings(experiment_name=experiment_name, output_dir=output_dir, debug=False) as gs:
+        # Parameters
+        x1 = FreeVariable(dimension=1)
+        parameters = Parameters(x1=x1)
+
+        # Setup QUEENS stuff
+        interface = DirectPythonInterface(
+            function="rosenbrock60_residual_1d", parameters=parameters
+        )
+        model = SimulationModel(interface=interface)
+        iterator = OptimizationIterator(
+            algorithm="LSQ",
+            initial_guess=[3.0],
+            result_description={"write_results": True},
+            bounds=[float("-inf"), float("inf")],
+            model=model,
+            parameters=parameters,
+        )
+
+        # Actual analysis
+        run_iterator(iterator)
+
+        # Load results
+        result_file = gs.output_dir / f"{gs.experiment_name}.pickle"
     with open(result_file, 'rb') as handle:
         results = pickle.load(handle)
     np.testing.assert_allclose(results.x, np.array([+1.0]))
