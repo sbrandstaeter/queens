ddbb910a93e33156eac9f94bdf1ac9a53aa1ef48
Dominik Rabe <d.rabe@unibw.de>
2024-04-10 17:55:44 +0200

    fix: switch input file to script test_monte_carlo_branin

--- tests/integration_tests/python/test_branin_monte_carlo.py
+++ tests/integration_tests/python/test_branin_monte_carlo.py
@@ -1,15 +1,48 @@
 """TODO_doc."""
+import pickle
+
 
 import pytest
 
-from queens.main import run
+from queens.distributions.uniform import UniformDistribution
+from queens.global_settings import GlobalSettings
+from queens.interfaces.direct_python_interface import DirectPythonInterface
+from queens.iterators.monte_carlo_iterator import MonteCarloIterator
+from queens.main import run_iterator
+from queens.models.simulation_model import SimulationModel
+from queens.parameters.parameters import Parameters
 from queens.utils.io_utils import load_result
 
 
-def test_branin_monte_carlo(inputdir, tmp_path):
+def test_branin_monte_carlo(tmp_path):
     """Test case for Monte Carlo iterator."""
-    run(inputdir / 'monte_carlo_branin.yml', tmp_path)
+    # Global settings
+    experiment_name = "monte_carlo_branin"
+    output_dir = tmp_path
 
-    results = load_result(tmp_path / 'xxx.pickle')
+    with GlobalSettings(experiment_name=experiment_name, output_dir=output_dir, debug=False) as gs:
+        # Parameters
+        x1 = UniformDistribution(lower_bound=-5, upper_bound=10)
+        x2 = UniformDistribution(lower_bound=0, upper_bound=15)
+        parameters = Parameters(x1=x1, x2=x2)
+
+        # Setup QUEENS stuff
+        interface = DirectPythonInterface(function="branin78_hifi", parameters=parameters)
+        model = SimulationModel(interface=interface)
+        iterator = MonteCarloIterator(
+            seed=42,
+            num_samples=1000,
+            result_description={"write_results": True, "plot_results": False},
+            model=model,
+            parameters=parameters,
+        )
+
+        # Actual analysis
+        run_iterator(iterator)
+
+        # Load results
+        result_file = gs.output_dir / f"{gs.experiment_name}.pickle"
+
+    results = load_result(result_file)
     assert results["mean"] == pytest.approx(55.81419875080866)
     assert results["var"] == pytest.approx(2754.1188056842070)
