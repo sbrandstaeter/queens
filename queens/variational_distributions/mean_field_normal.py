fix: wrong gradient

queens/variational_distributions/mean_field_normal.py
@@ -200,7 +200,7 @@ class MeanFieldNormalVariational(VariationalDistribution):
             total_grad (np.ndarray): Total Logpdf reparameterization gradient
         """
         total_grad = np.zeros((standard_normal_sample_batch.shape[0], variational_parameters.size))
-        total_grad[:, self.dimension :] = 1.0
+        total_grad[:, self.dimension :] = -1.0
         return total_grad
 
     def grad_sample_logpdf(self, variational_parameters, sample_batch):

tests/unit_tests/variational_distributions/test_variational_distributions.py
@@ -688,7 +688,9 @@ def test_total_grad_params_logpdf_mean_field(mean_field_distribution):
     std_samples = np.random.randn(2, mean_field_distribution.dimension)
     gradient = mean_field_distribution.total_grad_params_logpdf(variational_parameters, std_samples)
 
-    expected_gradient = np.array([[0.0, 0.0, 0.0, 1.0, 1.0, 1.0], [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]])
+    expected_gradient = np.array(
+        [[0.0, 0.0, 0.0, -1.0, -1.0, -1.0], [0.0, 0.0, 0.0, -1.0, -1.0, -1.0]]
+    )
     np.testing.assert_almost_equal(gradient, expected_gradient)
 
 