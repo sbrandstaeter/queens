c7a397310f5613b1cd3265896bb61c540eb93abb
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2024-05-17 16:32:31 +0200

    fix: remove double global_settings

--- tests/integration_tests/python/test_data_iterator.py
+++ tests/integration_tests/python/test_data_iterator.py
@@ -1,7 +1,6 @@
 """TODO_doc."""
 import pytest
 
-from queens.global_settings import GlobalSettings
 from queens.iterators.data_iterator import DataIterator
 from queens.main import run_iterator
 from queens.parameters.parameters import Parameters
@@ -11,9 +10,6 @@ from queens.utils.io_utils import load_result
 def test_branin_data_iterator(tmp_path, mocker, ref_result_iterator, _initialize_global_settings):
     """Test case for data iterator."""
     # Global settings
-    experiment_name = "branin_data_iterator"
-    output_dir = tmp_path
-
     output = {}
     output['result'] = ref_result_iterator
 
@@ -24,29 +20,28 @@ def test_branin_data_iterator(tmp_path, mocker, ref_result_iterator, _initialize
         return_value=[samples, output],
     )
 
-    with GlobalSettings(experiment_name=experiment_name, output_dir=output_dir, debug=False):
-        # Parameters
-        parameters = Parameters()
+    parameters = Parameters()
 
-        # Setup QUEENS stuff
-        iterator = DataIterator(
-            path_to_data="/path_to_data/some_data.pickle",
-            result_description={
-                "write_results": True,
-                "plot_results": False,
-                "num_support_points": 5,
-            },
-            parameters=parameters,
-            global_settings=_initialize_global_settings,
-        )
+    # Setup QUEENS stuff
+    iterator = DataIterator(
+        path_to_data="/path_to_data/some_data.pickle",
+        result_description={
+            "write_results": True,
+            "plot_results": False,
+            "num_support_points": 5,
+        },
+        parameters=parameters,
+        global_settings=_initialize_global_settings,
+    )
 
-        # Actual analysis
-        run_iterator(
-            iterator,
-            global_settings=_initialize_global_settings,
-        )
-        # Load results
-        result_file = output_dir / f"{experiment_name}.pickle"
-        results = load_result(result_file)
+    # Actual analysis
+    run_iterator(
+        iterator,
+        global_settings=_initialize_global_settings,
+    )
+    # Load results
+    result_file = tmp_path / "dummy_experiment_name.pickle"
+    results = load_result(result_file)
+
     assert results["mean"] == pytest.approx(1.3273452195599997)
     assert results["var"] == pytest.approx(44.82468751096612)
