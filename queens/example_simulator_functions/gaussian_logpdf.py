fix: log-likelihood shapes

queens/example_simulator_functions/gaussian_logpdf.py
@@ -53,7 +53,7 @@ def gaussian_1d_logpdf(x):
     Returns:
         float: The logpdf evaluated at *x*
     """
-    y = np.atleast_2d(STANDARD_NORMAL.logpdf(x))
+    y = STANDARD_NORMAL.logpdf(x)
     return y
 
 

queens/iterators/sequential_monte_carlo_iterator.py
@@ -157,9 +157,9 @@ class SequentialMonteCarloIterator(Iterator):
         # TODO: use normalised weights? # pylint: disable=fixme
         self.weights = np.ones((self.num_particles, 1))
 
-        self.log_likelihood = np.zeros((self.num_particles, 1))
-        self.log_prior = np.zeros((self.num_particles, 1))
-        self.log_posterior = np.zeros((self.num_particles, 1))
+        self.log_likelihood = np.zeros(self.num_particles)
+        self.log_prior = np.zeros(self.num_particles)
+        self.log_posterior = np.zeros(self.num_particles)
 
         self.ess = []
         self.ess_cur = 0.0
@@ -206,10 +206,10 @@ class SequentialMonteCarloIterator(Iterator):
         # draw initial particles from prior distribution
         self.particles = self.parameters.draw_samples(self.num_particles)
         self.log_likelihood = self.eval_log_likelihood(self.particles)
-        self.log_prior = self.eval_log_prior(self.particles).reshape(-1, 1)
+        self.log_prior = self.eval_log_prior(self.particles)
         self.log_posterior = self.log_likelihood + self.log_prior
         # initialize importance weights
-        self.weights = np.ones((self.num_particles, 1))
+        self.weights = np.ones(self.num_particles)
         self.ess_cur = self.num_particles
         self.ess.append(self.ess_cur)
 
@@ -343,13 +343,13 @@ class SequentialMonteCarloIterator(Iterator):
         """
         # draw from multinomial distribution to decide
         # the frequency of individual particles
-        particle_freq = np.random.multinomial(self.num_particles, np.squeeze(self.weights))
+        particle_freq = np.random.multinomial(self.num_particles, self.weights)
 
         idx_list = []
         for idx, freq in enumerate(particle_freq):
             idx_list += [idx] * freq
 
-        resampled_weights = np.ones((self.num_particles, 1))
+        resampled_weights = np.ones(self.num_particles)
 
         return (
             self.particles[idx_list],
@@ -398,7 +398,7 @@ class SequentialMonteCarloIterator(Iterator):
 
             # estimate current covariance matrix
             cov_mat = np.atleast_2d(
-                np.cov(self.particles, ddof=0, aweights=np.squeeze(self.weights), rowvar=False)
+                np.cov(self.particles, ddof=0, aweights=self.weights, rowvar=False)
             )
 
             # scale covariance based on average acceptance rate of last rejuvenation step

tests/unit_tests/models/test_bayesian_mf_gaussian_likelihood.py
@@ -323,7 +323,7 @@ def test_evaluate_mf_likelihood(default_mf_likelihood, mocker):
     assert distribution_mock.logpdf.call_count == diff_mat.shape[0]
 
     # test logpdf output and input
-    np.testing.assert_array_equal(log_lik_mf, np.array([[1], [1]]))
+    np.testing.assert_array_equal(log_lik_mf, np.array([1, 1]))
 
 
 def test_grad(default_mf_likelihood):