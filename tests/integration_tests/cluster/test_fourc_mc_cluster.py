c8d04ca69d98364b987ffad0743be0cd55230c65
Sebastian Brandstaeter <sebastian.brandstaeter@unibw.de>
2025-05-09 17:39:39 +0200

    fix: warning of argument before global argument in find

--- tests/integration_tests/cluster/test_fourc_mc_cluster.py
+++ tests/integration_tests/cluster/test_fourc_mc_cluster.py
@@ -195,7 +195,7 @@ class TestDaskCluster:
         command = (
             "find "
             + str(self.pytest_base_directory_on_cluster())
-            + " -mtime +7 -mindepth 1 -maxdepth 1 -type d -exec rm -rv {} \\;"
+            + " -mindepth 1 -maxdepth 1 -mtime +7 -type d -exec rm -rv {} \\;"
         )
         result = remote_connection.run(command, in_stream=False)
         _logger.debug("Deleting old simulation data:\n%s", result.stdout)
